daf314d439e772a75ccc886cdadbd7fb0af1b98c
Bill Lytton <billl@neurosim.downstate.edu>
2015-07-30 11:51:05 -0400

    grab a copy of init.py from mpiHHnet.py

--- sim/init.py
+++ sim/init.py
@@ -1,19 +1,30 @@
 # header
-'''Simulation to run a simple random network of excitatory 1 compartment Hodgkin-Huxley-ish p.cells based on Gillies and
-Starrett example on forum.  See readme.txt and exercises.txt'''
 import sys
 from neuron import h
-h.load_file('stdrun.hoc')
+from time import time
 import datetime
+h.load_file('stdrun.hoc')
 import params as p
-import scipy
+import shared as s
 
-# top level calls: read command line to pick up changes to params and to determine if called with -mpi
-ismpi = (p.nhost > 1) # assume that if only using 1 node it's to do viewing/debugging/analysis interactively
-from hhcell import hhcell
-import network
-## important that any calls that depend on params happen after commandline params are picked up
-[r.Random123_globalindex(p.runid) for r in p.randomizers] # can reset to prior p.runid to rerun
-## main run; this is the main call from top level -- will only be called if under MPI: if ismpi:runseq()
-network.runseq()
+def runSeq():
+    if s.rank==0: verystart=time()  # store initial time
+    s.sim.readArgs()  # set parameters based on commandline arguments
+    s.network.createPops()  # instantiate network populations
+    s.network.createCells()  # instantiate network cells based on defined populations
+    s.network.connectCells()  
+    s.network.addBackground()
+    s.sim.setupRecording()
+    s.sim.runSim()
+    s.sim.gatherData()
+    s.sim.saveData()
+    s.analysis.plotData()
+
+    if s.rank==0:
+        totaltime = time()-verystart # See how long it took in total
+        print('\nDone; total time = %0.1f s.' % totaltime)
+
+
+runSeq()
+
 print "DONE"

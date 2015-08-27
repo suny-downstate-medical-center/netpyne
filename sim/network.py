baede8c19edc39863ecbdaafc7933396d68e7afc
Salvador Dura <salvadordura@gmail.com>
2015-08-27 15:22:24 -0400

    combined shared.py and sim.py and converted to Class (but probably will turn back to module)

--- sim/network.py
+++ sim/network.py
@@ -7,87 +7,93 @@ Contains cell and population classes
 Contributors: salvadordura@gmail.com
 """
 
-from neuron import h  # import NEURON
 from pylab import seed, rand, sqrt, exp, transpose, ceil, concatenate, array, zeros, ones, vstack, show, disp, mean, inf, concatenate
 from time import time, sleep
 import pickle
 import warnings
-
-import params as p  # Import all shared variables and parameters
-import shared as s
+from neuron import h  # import NEURON
 
 warnings.filterwarnings('error')
 
+class Network(object):
 
-###############################################################################
-# Instantiate network populations (objects of class 'Pop')
-###############################################################################
-def createPops():
-    s.pops = []  # list to store populations ('Pop' objects)
-    for popParam in p.net['popParams']: # for each set of population parameters 
-        s.pops.append(s.Pop(popParam))  # instantiate a new object of class Pop and add to list pop
-
-
-###############################################################################
-# Create Cells
-###############################################################################
-def createCells():
-    s.pc.barrier()
-    if s.rank==0: print("\nCreating simulation of %i cell populations for %0.1f s on %i hosts..." % (len(s.pops),p.sim['duration']/1000.,s.nhosts)) 
-    s.gidVec=[] # Empty list for storing GIDs (index = local id; value = gid)
-    s.gidDic = {} # Empty dict for storing GIDs (key = gid; value = local id) -- ~x6 faster than gidVec.index()  
-    s.cells = []
-    for ipop in s.pops: # For each pop instantiate the network cells (objects of class 'Cell')
-        newCells = ipop.createCells() # create cells for this pop using Pop method
-        s.cells.extend(newCells)  # add to list of cells
-        s.pc.barrier()
-        if s.rank==0 and p.verbose: print('Instantiated %d cells of population %d'%(ipop.numCells, ipop.popgid))    
-    s.simdata.update({name:h.Vector(1e4).resize(0) for name in ['spkt','spkid']})
-    print('  Number of cells on node %i: %i ' % (s.rank,len(s.cells)))            
-    
-
-###############################################################################
-# Connect Cells
-###############################################################################
-def connectCells():
-    # Instantiate network connections (objects of class 'Conn') - connects object cells based on pre and post cell's type, class and yfrac
-    if s.rank==0: print('Making connections...'); connstart = time()
-
-    if p.net['connType'] == 'random':  # if random connectivity
-        connClass = s.RandConn  # select ConnRand class
-        arg = p.net['ncell']  # pass as argument num of presyn cell
-    
-    elif p.net['connType'] == 'yfrac':  # if yfrac-based connectivity
-        connClass = s.YfracConn  # select ConnYfrac class
-        data = [s.cells]*s.nhosts  # send cells data to other nodes
-        gather = s.pc.py_alltoall(data)  # collect cells data from other nodes (required to generate connections)
-        s.pc.barrier()
-        allCells = []
-        for x in gather:    allCells.extend(x)  # concatenate cells data from all nodes
-        del gather, data  # removed unnecesary variables
-        allCellsGids = [x.gid for x in allCells] # order gids
-        allCells = [x for (y,x) in sorted(zip(allCellsGids,allCells))]
-        arg = allCells  # pass as argument the list of presyn cell objects
-
-    s.conns = []  # list to store connections   
-    for ipost in s.cells: # for each postsynaptic cell in this node
-        newConns = connClass.connect(arg, ipost)  # calculate all connections
-        if newConns: s.conns.extend(newConns)  # add to list of connections in this node
-    
-    print('  Number of connections on host %i: %i ' % (s.rank, len(s.conns)))
-    s.pc.barrier()
-    if s.rank==0: conntime = time()-connstart; print('  Done; time = %0.1f s' % conntime) # See how long it took
+    ###############################################################################
+    # initialize variables
+    ###############################################################################
+    def __init__(self, params = None, sim = None):
+        self.params = params
+        self.sim = sim
 
 
-###############################################################################
-# Add background inputs
-###############################################################################
-def addBackground():
-    if s.rank==0: print('Creating background inputs...')
-    for c in s.cells: 
-        c.addBackground()
-    print('  Number created on host %i: %i' % (s.rank, len(s.cells)))
-    s.pc.barrier()
+    ###############################################################################
+    # Instantiate network populations (objects of class 'Pop')
+    ###############################################################################
+    def createPops(self):
+        self.pops = []  # list to store populations ('Pop' objects)
+        for popParam in self.params['popParams']: # for each set of population parameters 
+            self.pops.append(self.Pop(popParam))  # instantiate a new object of class Pop and add to list pop
+
+
+    ###############################################################################
+    # Create Cells
+    ###############################################################################
+    def createCells(self):
+        self.sim.pc.barrier()
+        if self.rank==0: print("\nCreating simulation of %i cell populations for %0.1f s on %i hosts..." % (len(self.pops), self.sim.params['duration']/1000.,self.sim.nhosts)) 
+        self.gidVec = [] # Empty list for storing GIDs (index = local id; value = gid)
+        self.gidDic = {} # Empty dict for storing GIDs (key = gid; value = local id) -- ~x6 faster than gidVec.index()  
+        self.cells = []
+        for ipop in self.pops: # For each pop instantiate the network cells (objects of class 'Cell')
+            newCells = ipop.createCells() # create cells for this pop using Pop method
+            self.cells.extend(newCells)  # add to list of cells
+            self.sim.pc.barrier()
+            if self.rank==0 and p.verbose: print('Instantiated %d cells of population %d'%(ipop.numCells, ipop.popgid))    
+        self.simdata.update({name:h.Vector(1e4).resize(0) for name in ['spkt','spkid']})
+        print('  Number of cells on node %i: %i ' % (self.rank,len(self.cells)))            
+        
+
+    ###############################################################################
+    # Connect Cells
+    ###############################################################################
+    def connectCells(self):
+        # Instantiate network connections (objects of class 'Conn') - connects object cells based on pre and post cell's type, class and yfrac
+        if self.rank==0: print('Making connections...'); connstart = time()
+
+        if self.params['connType'] == 'random':  # if random connectivity
+            connClass = self.RandConn  # select ConnRand class
+            arg = self.params['ncell']  # pass as argument num of presyn cell
+        
+        elif self.params['connType'] == 'yfrac':  # if yfrac-based connectivity
+            connClass = self.YfracConn  # select ConnYfrac class
+            data = [s.cells]*s.nhosts  # send cells data to other nodes
+            gather = self.pc.py_alltoall(data)  # collect cells data from other nodes (required to generate connections)
+            self.pc.barrier()
+            allCells = []
+            for x in gather:    allCells.extend(x)  # concatenate cells data from all nodes
+            del gather, data  # removed unnecesary variables
+            allCellsGids = [x.gid for x in allCells] # order gids
+            allCells = [x for (y,x) in sorted(zip(allCellsGids,allCells))]
+            arg = allCells  # pass as argument the list of presyn cell objects
+
+        self.conns = []  # list to store connections   
+        for ipost in self.cells: # for each postsynaptic cell in this node
+            newConns = connClass.connect(arg, ipost)  # calculate all connections
+            if newConns: self.conns.extend(newConns)  # add to list of connections in this node
+        
+        print('  Number of connections on host %i: %i ' % (self.rank, len(self.conns)))
+        self.pc.barrier()
+        if self.rank==0: conntime = time()-connstart; print('  Done; time = %0.1f s' % conntime) # See how long it took
+
+
+    ###############################################################################
+    # Add background inputs
+    ###############################################################################
+    def addBackground(self):
+        if self.rank==0: print('Creating background inputs...')
+        for c in self.cells: 
+            c.addBackground()
+        print('  Number created on host %i: %i' % (self.rank, len(self.cells)))
+        self.pc.barrier()
 
 
 
--- sim/sim.py
+++ sim/sim.py
@@ -14,151 +14,181 @@ from datetime import datetime
 import pickle
 import cPickle as pk
 from neuron import h, init # Import NEURON
-import params as p
-import shared as s
+import analysis
 
-###############################################################################
-### Update model parameters from command-line arguments
-###############################################################################
-def readArgs():
-    for argv in sys.argv[1:]: # Skip first argument, which is file name
-        arg = argv.replace(' ','').split('=') # ignore spaces and find varname and value
-        harg = arg[0].split('.')+[''] # Separate out variable name; '' since if split fails need to still have an harg[1]
-        if len(arg)==2:
-            if hasattr(p,arg[0]) or hasattr(p,harg[1]): # Check that variable exists
-                if arg[0] == 'outfilestem':
-                    exec('s.'+arg[0]+'="'+arg[1]+'"') # Actually set variable 
-                    if s.rank==0: # messages only come from Master  
-                        print('  Setting %s=%s' %(arg[0],arg[1]))
-                else:
-                    exec('p.'+argv) # Actually set variable            
-                    if s.rank==0: # messages only come from Master  
-                        print('  Setting %s=%r' %(arg[0],eval(arg[1])))
-            else: 
-                sys.tracebacklimit=0
-                raise Exception('Reading args from commandline: Variable "%s" not found' % arg[0])
-        elif argv=='-mpi':   s.ismpi = True
-        else: pass # ignore -python 
+class Simulation(object):
+
+    ###############################################################################
+    # initialize variables and 
+    ###############################################################################
+    def __init__(self, params=None, net=None):
+        self.simdata = {}
+        self.simdataVecs = ['spkt', 'spkid']
+        self.lastGid = 0  # keep track of las cell gid
+        self.fih = []  # list of func init handlers
+        self.params = params
+        self.net = net
+        self.initMPI()
 
 
-###############################################################################
-### Setup Recording
-###############################################################################
-def setupRecording():
-    # spike recording
-    s.pc.spike_record(-1, s.simdata['spkt'], s.simdata['spkid']) # -1 means to record from all cells on this node
+    def id32(obj): 
+        return int(hashlib.md5(obj).hexdigest()[0:8],16)# hash(obj) & 0xffffffff # for random seeds (bitwise AND to retain only lower 32 bits)
 
-    # background inputs recording
-    if p.sim['saveBackground']:
-        for c in s.cells:
-            s.backgroundSpikevecs=[] # A list for storing actual cell voltages (WARNING, slow!)
-            s.backgroundRecorders=[] # And for recording spikes
-            backgroundSpikevec = h.Vector() # Initialize vector
-            s.backgroundSpikevecs.append(backgroundSpikevec) # Keep all those vectors
-            backgroundRecorder = h.NetCon(c.backgroundSource, None)
-            backgroundRecorder.record(backgroundSpikevec) # Record simulation time
-            s.backgroundRecorders.append(backgroundRecorder)
+    ###############################################################################
+    # Init MPI
+    ###############################################################################
+    def initMPI:
+        self.pc = h.ParallelContext() # MPI: Initialize the ParallelContext class
+        self.nhosts = int(pc.nhost()) # Find number of hosts
+        self.rank = int(pc.id())     # rank 0 will be the master
 
-    # intrinsic cell variables recording
-    if p.sim['recordTraces']:
-        #s.simdataVecs.extend(p.sim['recdict'].keys())
-        for k in p.sim['recdict'].keys(): s.simdata[k] = {}
-        for c in s.cells: 
-            c.record()
-
-###############################################################################
-### Run Simulation
-###############################################################################
-def runSim():
-    if s.rank == 0:
-        print('\nRunning...')
-        runstart = time() # See how long the run takes
-    s.pc.set_maxstep(10)
-    mindelay = s.pc.allreduce(s.pc.set_maxstep(10), 2) # flag 2 returns minimum value
-    if s.rank==0: print 'Minimum delay (time-step for queue exchange) is ',mindelay
-    init() # 
-    #h.cvode.event(s.savestep,savenow)
-    s.pc.psolve(p.sim['tstop'])
-    if s.rank==0: 
-        runtime = time()-runstart # See how long it took
-        print('  Done; run time = %0.1f s; real-time ratio: %0.2f.' % (runtime, p.sim['duration']/1000/runtime))
-    s.pc.barrier() # Wait for all hosts to get to this point
+        if self.rank==0: 
+            print('\nSetting parameters...')
+            pc.gid_clear()
 
 
+    ###############################################################################
+    ### Update model parameters from command-line arguments
+    ###############################################################################
+    def readArgs(self):
+        for argv in sys.argv[1:]: # Skip first argument, which is file name
+            arg = argv.replace(' ','').split('=') # ignore spaces and find varname and value
+            harg = arg[0].split('.')+[''] # Separate out variable name; '' since if split fails need to still have an harg[1]
+            if len(arg)==2:
+                if hasattr(p,arg[0]) or hasattr(p,harg[1]): # Check that variable exists
+                    if arg[0] == 'outfilestem':
+                        exec('s.'+arg[0]+'="'+arg[1]+'"') # Actually set variable 
+                        if s.rank==0: # messages only come from Master  
+                            print('  Setting %s=%s' %(arg[0],arg[1]))
+                    else:
+                        exec('p.'+argv) # Actually set variable            
+                        if s.rank==0: # messages only come from Master  
+                            print('  Setting %s=%r' %(arg[0],eval(arg[1])))
+                else: 
+                    sys.tracebacklimit=0
+                    raise Exception('Reading args from commandline: Variable "%s" not found' % arg[0])
+            elif argv=='-mpi':   s.ismpi = True
+            else: pass # ignore -python 
 
-###############################################################################
-### Gather data from nodes
-###############################################################################
-def gatherData():
-    ## Pack data from all hosts
-    if s.rank==0: 
-        print('\nGathering spikes...')
-        gatherstart = time() # See how long it takes to plot
 
-    # extend simdata dictionary to save relevant data in each node
-    nodePops = [[y.__dict__[x] for x in y.__dict__ if not x in ['density']] for y in s.pops]
-    nodeCells = [[y.__dict__[x] for x in y.__dict__ if not x in ['soma', 'stim', 'sec', 'm', 'syns', 'backgroundSyn', 'backgroundSource', 'backgroundConn', 'backgroundRand']] for y in s.cells]
-    nodeConns = [[y.__dict__[x] for x in y.__dict__ if not x in ['netcon', 'connWeights', 'connProbs']] for y in s.conns]
-    s.simdata.update({'pops': nodePops, 'cells': nodeCells, 'conns': nodeConns})
-    if p.sim['saveBackground']:
-        nodeBackground = [(s.cells[i].gid, s.backgroundSpikevecs[i]) for i in range(s.cells)]
-        s.simdata.update({'background': nodeBackground})  
+    ###############################################################################
+    ### Setup Recording
+    ###############################################################################
+    def setupRecording(self):
+        # spike recording
+        self.pc.spike_record(-1, self.simdata['spkt'], self.simdata['spkid']) # -1 means to record from all cells on this node
 
-    # gather simdata from all nodes
-    data=[None]*s.nhosts # using None is important for mem and perf of pc.alltoall() when data is sparse
-    data[0]={} # make a new dict
-    for k,v in s.simdata.iteritems():   data[0][k] = v 
-    gather=s.pc.py_alltoall(data)
-    s.pc.barrier()
-    #for v in s.simdata.itervalues(): v.resize(0) # 
-    if s.rank==0: 
-        s.allsimdata = {}
-        [s.allsimdata.update({k : concatenate([array(d[k]) for d in gather])}) for k in s.simdataVecs] # concatenate spikes
-        for k in [x for x in gather[0].keys() if x not in s.simdataVecs]: # concatenate other dict fields
-            tmp = []
-            for d in gather: tmp.extend(d[k]) 
-            s.allsimdata.update({k: array(tmp, dtype=object)})  # object type so can be saved in .mat format
-        if not s.allsimdata.has_key('run'): s.allsimdata.update({'run':{'recordStep':p.sim['recordStep'], 'dt':h.dt, 'randseed':p.sim['randseed'], 'duration':p.sim['duration']}}) # add run params
-        s.allsimdata.update({'t':h.t, 'walltime':datetime.now().ctime()})
+        # background inputs recording
+        if self.params['saveBackground']:
+            for c in self.net.cells:
+                self.backgroundSpikevecs=[] # A list for storing actual cell voltages (WARNING, slow!)
+                self.backgroundRecorders=[] # And for recording spikes
+                backgroundSpikevec = h.Vector() # Initialize vector
+                self.backgroundSpikevecs.append(backgroundSpikevec) # Keep all those vectors
+                backgroundRecorder = h.NetCon(c.backgroundSource, None)
+                backgroundRecorder.record(backgroundSpikevec) # Record simulation time
+                self.backgroundRecorders.append(backgroundRecorder)
 
-        gathertime = time()-gatherstart # See how long it took
-        print('  Done; gather time = %0.1f s.' % gathertime)
+        # intrinsic cell variables recording
+        if self.params['recordTraces']:
+            #s.simdataVecs.extend(p.sim['recdict'].keys())
+            for k in self.params['recdict'].keys(): self.simdata[k] = {}
+            for c in self.net.cells: 
+                c.record()
 
-    ## Print statistics
-    if s.rank == 0:
-        print('\nAnalyzing...')
-        s.totalspikes = len(s.allsimdata['spkt'])    
-        s.totalconnections = len(s.allsimdata['conns'])
-        s.ncells = len(s.allsimdata['cells'])
+    ###############################################################################
+    ### Run Simulation
+    ###############################################################################
+    def runSim(self):
+        if self.rank == 0:
+            print('\nRunning...')
+            runstart = time() # See how long the run takes
+        self.pc.set_maxstep(10)
+        mindelay = self.pc.allreduce(self.pc.set_maxstep(10), 2) # flag 2 returns minimum value
+        if self.rank==0: print 'Minimum delay (time-step for queue exchange) is ',mindelay
+        init() # 
+        #h.cvode.event(s.savestep,savenow)
+        self.pc.psolve(p.sim['tstop'])
+        if self.rank==0: 
+            runtime = time()-runstart # See how long it took
+            print('  Done; run time = %0.1f s; real-time ratio: %0.2f.' % (runtime, self.params['duration']/1000/runtime))
+        self.pc.barrier() # Wait for all hosts to get to this point
 
-        s.firingrate = float(s.totalspikes)/s.ncells/p.sim['duration']*1e3 # Calculate firing rate 
-        s.connspercell = s.totalconnections/float(s.ncells) # Calculate the number of connections per cell
-        print('  Run time: %0.1f s (%i-s sim; %i scale; %i cells; %i workers)' % (gathertime, p.sim['duration']/1e3, p.net['scale'], s.ncells, s.nhosts))
-        print('  Spikes: %i (%0.2f Hz)' % (s.totalspikes, s.firingrate))
-        print('  Connections: %i (%0.2f per cell)' % (s.totalconnections, s.connspercell))
- 
 
-###############################################################################
-### Save data
-###############################################################################
-def saveData():
-    if s.rank == 0:
-        print('Saving output as %s...' % p.sim['filename'])
-        # Save to dpk file
-        if p.sim['savedpk']:
-            import os,gzip
-            fn=p.sim['filename'].split('.')
-            file='{}{:d}.{}'.format(fn[0],int(round(h.t)),fn[1]) # insert integer time into the middle of file name
-            gzip.open(file, 'wb').write(pk.dumps(s.allsimdata)) # write compressed string
-            print 'Wrote file {}/{} of size {:.3f} MB'.format(os.getcwd(),file,os.path.getsize(file)/1e6)
-  
-        # Save to mat file
-        if p.sim['savemat']:
-            savestart = time() # See how long it takes to save
-            from scipy.io import savemat # analysis:ignore -- because used in exec() statement
-            savemat(p.sim['filename'], s.allsimdata)  # check if looks ok in matlab
-            savetime = time()-savestart # See how long it took to save
-            print('  Done; time = %0.1f s' % savetime)
 
-        # check shelves
+    ###############################################################################
+    ### Gather data from nodes
+    ###############################################################################
+    def gatherData(self):
+        ## Pack data from all hosts
+        if self.rank==0: 
+            print('\nGathering spikes...')
+            gatherstart = time() # See how long it takes to plot
+
+        # extend simdata dictionary to save relevant data in each node
+        nodePops = [[y.__dict__[x] for x in y.__dict__ if not x in ['density']] for y in self.net.pops]
+        nodeCells = [[y.__dict__[x] for x in y.__dict__ if not x in ['soma', 'stim', 'sec', 'm', 'syns', 'backgroundSyn', 'backgroundSource', 'backgroundConn', 'backgroundRand']] for y in s.cells]
+        nodeConns = [[y.__dict__[x] for x in y.__dict__ if not x in ['netcon', 'connWeights', 'connProbs']] for y in self.net.conns]
+        self.simdata.update({'pops': nodePops, 'cells': nodeCells, 'conns': nodeConns})
+        if p.sim['saveBackground']:
+            nodeBackground = [(self.net.cells[i].gid, self.backgroundSpikevecs[i]) for i in range(self.net.cells)]
+            self.simdata.update({'background': nodeBackground})  
+
+        # gather simdata from all nodes
+        data=[None]*self.nhosts # using None is important for mem and perf of pc.alltoall() when data is sparse
+        data[0]={} # make a new dict
+        for k,v in self.simdata.iteritems():   data[0][k] = v 
+        gather=s.pc.py_alltoall(data)
+        s.pc.barrier()
+        #for v in s.simdata.itervalues(): v.resize(0) # 
+        if s.rank==0: 
+            s.allsimdata = {}
+            [s.allsimdata.update({k : concatenate([array(d[k]) for d in gather])}) for k in s.simdataVecs] # concatenate spikes
+            for k in [x for x in gather[0].keys() if x not in s.simdataVecs]: # concatenate other dict fields
+                tmp = []
+                for d in gather: tmp.extend(d[k]) 
+                s.allsimdata.update({k: array(tmp, dtype=object)})  # object type so can be saved in .mat format
+            if not s.allsimdata.has_key('run'): s.allsimdata.update({'run':{'recordStep':p.sim['recordStep'], 'dt':h.dt, 'randseed':p.sim['randseed'], 'duration':p.sim['duration']}}) # add run params
+            s.allsimdata.update({'t':h.t, 'walltime':datetime.now().ctime()})
+
+            gathertime = time()-gatherstart # See how long it took
+            print('  Done; gather time = %0.1f s.' % gathertime)
+
+        ## Print statistics
+        if s.rank == 0:
+            print('\nAnalyzing...')
+            s.totalspikes = len(s.allsimdata['spkt'])    
+            s.totalconnections = len(s.allsimdata['conns'])
+            s.ncells = len(s.allsimdata['cells'])
+
+            s.firingrate = float(s.totalspikes)/s.ncells/p.sim['duration']*1e3 # Calculate firing rate 
+            s.connspercell = s.totalconnections/float(s.ncells) # Calculate the number of connections per cell
+            print('  Run time: %0.1f s (%i-s sim; %i scale; %i cells; %i workers)' % (gathertime, p.sim['duration']/1e3, p.net['scale'], s.ncells, s.nhosts))
+            print('  Spikes: %i (%0.2f Hz)' % (s.totalspikes, s.firingrate))
+            print('  Connections: %i (%0.2f per cell)' % (s.totalconnections, s.connspercell))
+     
+
+    ###############################################################################
+    ### Save data
+    ###############################################################################
+    def saveData():
+        if s.rank == 0:
+            print('Saving output as %s...' % p.sim['filename'])
+            # Save to dpk file
+            if p.sim['savedpk']:
+                import os,gzip
+                fn=p.sim['filename'].split('.')
+                file='{}{:d}.{}'.format(fn[0],int(round(h.t)),fn[1]) # insert integer time into the middle of file name
+                gzip.open(file, 'wb').write(pk.dumps(s.allsimdata)) # write compressed string
+                print 'Wrote file {}/{} of size {:.3f} MB'.format(os.getcwd(),file,os.path.getsize(file)/1e6)
+      
+            # Save to mat file
+            if p.sim['savemat']:
+                savestart = time() # See how long it takes to save
+                from scipy.io import savemat # analysis:ignore -- because used in exec() statement
+                savemat(p.sim['filename'], s.allsimdata)  # check if looks ok in matlab
+                savetime = time()-savestart # See how long it took to save
+                print('  Done; time = %0.1f s' % savetime)
+
+            # check shelves
 

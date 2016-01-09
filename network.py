b65e14c53a7a2d985864fd06c1777b827c1e2caf
salvadord <salvadordura@gmail.com>
2016-01-09 12:40:48 -0500

    fixed randomization in randConn

--- network.py
+++ network.py
@@ -151,6 +151,7 @@ class Network(object):
         for postCellGid, postCell in postCells.iteritems():  # for each postsyn cell
             preCellGids = preCells.keys()
             if postCellGid in preCellGids: preCellGids.remove(postCellGid)
+            random.seed(f.sim.id32('%d'%(f.cfg['randseed']+postCells.keys()[0]))) 
             randPreCellGids = random.sample(preCellGids, random.randint(0, min(connParam['maxConns'], len(preCellGids)))) # select random subset of pre cells
             for randPreCellGid in randPreCellGids: # for each presyn cell
                 if randDelays:  delay = randDelays.pop()  # set random delay

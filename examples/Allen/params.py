4517446fc8c362dd484ee5c0de2e100fa481589e
salvadord <salvadordura@gmail.com>
2016-07-19 21:19:25 -0400

    updated code to read Allen cells and added actual files to repo

--- examples/Allen/params.py
+++ examples/Allen/params.py
@@ -1,6 +1,9 @@
-from netpyne import specs, sim
+from netpyne import specs
 
-# Network parameters
+###############################################################################
+# NETWORK PARAMETERS
+###############################################################################
+
 netParams = specs.NetParams()  # object of class NetParams to store the network parameters
 
 netParams.sizeX = 50 # x-dimension (horizontal length) size in um
@@ -9,33 +12,20 @@ netParams.sizeZ = 50 # z-dimension (horizontal length) size in um
 netParams.propVelocity = 100.0 # propagation velocity (um/ms)
 netParams.probLengthConst = 150.0 # length constant for conn probability (um)
 
-
 ## Population parameters
-netParams.addPopParams('E2',  {'cellType': 'E', 'density': 50, 'ynormRange': [100,300], 'cellModel': 'HH'}) 
-netParams.addPopParams('IF2', {'cellType': 'I', 'density': 50, 'ynormRange': [100,300], 'cellModel': 'HH'}) 
-netParams.addPopParams('IL2', {'cellType': 'I', 'density': 50, 'ynormRange': [100,300], 'cellModel': 'HH'}) 
-netParams.addPopParams('E4',  {'cellType': 'E', 'density': 50, 'ynormRange': [300,600], 'cellModel': 'HH'}) 
-netParams.addPopParams('IF4', {'cellType': 'I', 'density': 50, 'ynormRange': [300,600], 'cellModel': 'HH'}) 
-netParams.addPopParams('IL4', {'cellType': 'I', 'density': 50, 'ynormRange': [300,600], 'cellModel': 'HH'}) 
-netParams.addPopParams('E5',  {'cellType': 'E', 'density': 50, 'ynormRange': [0.6,1.0], 'cellModel': 'HH'}) 
-netParams.addPopParams('IF5', {'cellType': 'I', 'density': 50, 'ynormRange': [0.6,1.0], 'cellModel': 'HH'}) 
-netParams.addPopParams('IL5', {'cellType': 'I', 'density': 50, 'ynormRange': [0.6,1.0], 'cellModel': 'HH'}) 
+netParams.addPopParams('L2_E',  {'cellType': 'E2', 'density': 50, 'ynormRange': [100,300], 'cellModel': 'HH'}) 
+netParams.addPopParams('L2_IF', {'cellType': 'IF', 'density': 50, 'ynormRange': [100,300], 'cellModel': 'HH'}) 
+netParams.addPopParams('L2_IL', {'cellType': 'IL', 'density': 50, 'ynormRange': [100,300], 'cellModel': 'HH'}) 
+netParams.addPopParams('L4_E',  {'cellType': 'E4', 'density': 50, 'ynormRange': [300,600], 'cellModel': 'HH'}) 
+netParams.addPopParams('L4_IF', {'cellType': 'IF', 'density': 50, 'ynormRange': [300,600], 'cellModel': 'HH'}) 
+netParams.addPopParams('L4_IL', {'cellType': 'IL', 'density': 50, 'ynormRange': [300,600], 'cellModel': 'HH'}) 
+netParams.addPopParams('L5_E',  {'cellType': 'E5', 'density': 50, 'ynormRange': [0.6,1.0], 'cellModel': 'HH'}) 
+netParams.addPopParams('L5_IF', {'cellType': 'IF', 'density': 50, 'ynormRange': [0.6,1.0], 'cellModel': 'HH'}) 
+netParams.addPopParams('L5_IL', {'cellType': 'IL', 'density': 50, 'ynormRange': [0.6,1.0], 'cellModel': 'HH'}) 
 netParams.addPopParams('background', {'rate': 20, 'noise': 0.3, 'cellModel': 'NetStim'})
 
-
 ## Cell property rules
-cellRule = {'conds': {'cellType': 'E'},  'secs': {}}  # cell rule dict
-cellRule['secs']['soma'] = {'geom': {}, 'mechs': {}}                              # soma params dict
-cellRule['secs']['soma']['geom'] = {'diam': 15, 'L': 14, 'Ra': 120.0}                   # soma geometry
-cellRule['secs']['soma']['mechs']['hh'] = {'gnabar': 0.13, 'gkbar': 0.036, 'gl': 0.003, 'el': -70}      # soma hh mechanism
-netParams.addCellParams('Erule', cellRule)                          # add dict to list of cell params
-
-cellRule = {'conds': {'cellType': 'I'},  'secs': {}}  # cell rule dict
-cellRule['secs']['soma'] = {'geom': {}, 'mechs': {}}                              # soma params dict
-cellRule['secs']['soma']['geom'] = {'diam': 10.0, 'L': 9.0, 'Ra': 110.0}                  # soma geometry
-cellRule['secs']['soma']['mechs']['hh'] = {'gnabar': 0.11, 'gkbar': 0.036, 'gl': 0.003, 'el': -70}      # soma hh mechanism
-netParams.addCellParams('Irule', cellRule)                          # add dict to list of cell params
-
+netParams.importCellParams(label='E2_rule', conds={'cellType': 'PYR', 'cellModel': 'HH'}, fileName='getCells.py', cellName='E2')
 
 ## Synaptic mechanism parameters
 netParams.addSynMechParams('exc', {'mod': 'Exp2Syn', 'tau1': 0.8, 'tau2': 5.3, 'e': 0})  # NMDA synaptic mechanism
@@ -43,28 +33,31 @@ netParams.addSynMechParams('inh', {'mod': 'Exp2Syn', 'tau1': 0.6, 'tau2': 8.5, '
  
 
 ## Cell connectivity rules
-netParams.addConnParams('bg->all',
-{'preConds': {'popLabel': 'background'}, 'postConds': {'cellType': ['E', 'I']}, # background -> all
-  'weight': 0.01,                     # synaptic weight 
-  'delay': 'max(1, gauss(5,2))',      # transmission delay (ms) 
-  'synMech': 'exc'})                  # synaptic mechanism 
+# netParams.addConnParams('bg->all',
+# {'preConds': {'popLabel': 'background'}, 'postConds': {'cellType': ['E', 'I']}, # background -> all
+#   'weight': 0.01,                     # synaptic weight 
+#   'delay': 'max(1, gauss(5,2))',      # transmission delay (ms) 
+#   'synMech': 'exc'})                  # synaptic mechanism 
 
-netParams.addConnParams('E->all',
-{'preConds': {'cellType': 'E'}, 'postConds': {'y': [100,1000]},  #  E -> all (100-1000 um)
-  'probability': 0.1 ,                  # probability of connection
-  'weight': '0.005*post_ynorm',         # synaptic weight 
-  'delay': 'dist_3D/propVelocity',      # transmission delay (ms) 
-  'synMech': 'exc'})                    # synaptic mechanism 
+# netParams.addConnParams('E->all',
+# {'preConds': {'cellType': 'E'}, 'postConds': {'y': [100,1000]},  #  E -> all (100-1000 um)
+#   'probability': 0.1 ,                  # probability of connection
+#   'weight': '0.005*post_ynorm',         # synaptic weight 
+#   'delay': 'dist_3D/propVelocity',      # transmission delay (ms) 
+#   'synMech': 'exc'})                    # synaptic mechanism 
 
-netParams.addConnParams('I->E',
-{'preConds': {'cellType': 'I'}, 'postConds': {'popLabel': ['E2','E4','E5']},       #  I -> E
-  'probability': '0.4*exp(-dist_3D/probLengthConst)',   # probability of connection
-  'weight': 0.001,                                     # synaptic weight 
-  'delay': 'dist_3D/propVelocity',                    # transmission delay (ms) 
-  'synMech': 'inh'})                                  # synaptic mechanism 
+# netParams.addConnParams('I->E',
+# {'preConds': {'cellType': 'I'}, 'postConds': {'popLabel': ['E2','E4','E5']},       #  I -> E
+#   'probability': '0.4*exp(-dist_3D/probLengthConst)',   # probability of connection
+#   'weight': 0.001,                                     # synaptic weight 
+#   'delay': 'dist_3D/propVelocity',                    # transmission delay (ms) 
+#   'synMech': 'inh'})                                  # synaptic mechanism 
 
 
-# Simulation options
+###############################################################################
+# SIMULATION CONFIGURATION
+###############################################################################
+
 simConfig = specs.SimConfig()        # object of class SimConfig to store simulation configuration
 simConfig.duration = 1*1e3           # Duration of the simulation, in ms
 simConfig.dt = 0.05                 # Internal integration timestep to use
@@ -75,10 +68,10 @@ simConfig.filename = 'model_output'  # Set file output name
 simConfig.savePickle = True        # Save params, network and sim output to pickle file
 simConfig.timestampFilename = True
 
-simConfig.addAnalysis('plotRaster', {'orderBy': 'y', 'orderInverse': True, 'saveFig': True})      # Plot a raster
-simConfig.addAnalysis('plotTraces', {'include': [('E2',0), ('E4', 0), ('E5', 5)]})      # Plot recorded traces for this list of cells
-simConfig.addAnalysis('plot2Dnet', True)           # plot 2D visualization of cell positions and connections
-simConfig.addAnalysis('plotConn', True)           # plot connectivity matrix
+# simConfig.addAnalysis('plotRaster', {'orderBy': 'y', 'orderInverse': True, 'saveFig': True})      # Plot a raster
+# simConfig.addAnalysis('plotTraces', {'include': [('E2',0), ('E4', 0), ('E5', 5)]})      # Plot recorded traces for this list of cells
+# simConfig.addAnalysis('plot2Dnet', True)           # plot 2D visualization of cell positions and connections
+# simConfig.addAnalysis('plotConn', True)           # plot connectivity matrix
 
 # Create network and run simulation
 sim.createSimulateAnalyze(netParams = netParams, simConfig = simConfig)    

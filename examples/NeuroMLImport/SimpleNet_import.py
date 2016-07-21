87903f91362ea1bfe816fcd2adb9ea1110378e4b
Padraig Gleeson <p.gleeson@gmail.com>
2016-07-21 16:44:16 +0100

    Tested with parallel neuron

--- examples/NeuroMLImport/SimpleNet_import.py
+++ examples/NeuroMLImport/SimpleNet_import.py
@@ -10,7 +10,7 @@ file_name = 'SimpleNet.net.nml'
 simConfig = specs.SimConfig()  # dictionary to store simConfig
 
 # Simulation parameters
-simConfig.duration = 1000 # Duration of the simulation, in ms
+simConfig.duration = 10000 # Duration of the simulation, in ms
 simConfig.dt = 0.025 # Internal integration timestep to use
 simConfig.verbose = True
 

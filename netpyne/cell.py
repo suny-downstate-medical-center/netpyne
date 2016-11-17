6228615ad18f4ef062fed68149b69a36e35bb500
salvadord <salvadordura@gmail.com>
2016-11-17 09:59:41 -0800

    removed temporal fix in cell.py to set ypos of cell at -735

--- netpyne/cell.py
+++ netpyne/cell.py
@@ -184,12 +184,7 @@ class Cell (object):
                 if 'pt3d' in sectParams['geom']:  
                     h.pt3dclear(sec=sec['hSec'])
                     x = self.tags['x']
-                    #y = -self.tags['y'] # Neuron y-axis positive = upwards, so assume pia=0 and cortical depth = neg
-
-                    ############################################################################
-                    y = -735  # TEMPORARILY FIX THIS FOR SFN16 EXPERIMENT!! - REMEMBER TO REMOVE!!!!!!!
-                    ############################################################################
-                    
+                    y = -self.tags['y'] # Neuron y-axis positive = upwards, so assume pia=0 and cortical depth = neg
                     z = self.tags['z']
                     for pt3d in sectParams['geom']['pt3d']:
                         h.pt3dadd(x+pt3d[0], y+pt3d[1], z+pt3d[2], pt3d[3], sec=sec['hSec'])

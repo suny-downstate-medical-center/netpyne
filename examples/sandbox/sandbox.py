99c235399c57c6c10e03fa6c4295b9acd01b51f8
salvadord <salvadordura@gmail.com>
2017-06-12 15:13:20 -0400

    debugging issue with plotTraces

--- examples/sandbox/sandbox.py
+++ examples/sandbox/sandbox.py
@@ -5,12 +5,12 @@ h.load_file('stdrun.hoc')
 pc = h.ParallelContext()
 pc.set_maxstep(10)
 
-h.dt = 0.1
+h.dt = 0.2
 tstop = 1.0
 s = h.Section()
 t = h.Vector()
 v = h.Vector()
-v.record(s(0.5)._v_)
+v.record(s(0.5)._ref_v)
 #t.record(h._ref_t)
 
 h.stdinit()

2d185f6b5c08908c257741489d53a2dc82f60fcf
Siddhartha Mitra <smitra@Siddharthas-Air.office.42west24.com>
2017-08-06 22:40:48 -0400

    working version final

--- netpyne/specs.py
+++ netpyne/specs.py
@@ -499,7 +499,7 @@ class SimConfig (object):
         self.saveCellConns = True  # save all the conns info for each cell (False reduces time+space; prevents re-simulation)
 
         # error checking
-        self.checkErrors = True # whether to validate the input parameters
+        self.checkErrors = False # whether to validate the input parameters
         self.checkErrorsVerbose = False # whether to print detailed errors during input parameter validation
         # self.exitOnError = False # whether to hard exit on error
 

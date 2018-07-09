b9e55cacffebf3f9bfda016089440f6806d31099
salvadord <salvadordura@gmail.com>
2018-07-09 10:57:13 -0400

    started adding support for rxd: species, regions, and concentration plot

--- netpyne/analysis.py
+++ netpyne/analysis.py
@@ -18,6 +18,7 @@ from numbers import Number
 import math
 import functools
 from support.scalebar import add_scalebar
+from matplotlib_scalebar import scalebar
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -3389,3 +3390,28 @@ def plotEPSPAmp(include=None, trace=None, start=0, interval=50, number=2, amp='a
 
     return peaks, fig
 
+######################################################################################################################################################
+## Plot RxD concentration
+######################################################################################################################################################
+@exception
+def plotRxDConcentration(speciesLabel, regionLabel, plane='xy', showFig=True):
+    import sim
+    species = sim.net.rxd['species'][speciesLabel]
+    region = sim.net.rxd['regions'][regionLabel]
+    fig=plt.figure(figsize=(4,10))
+    plane2mean = {'xz': 1, 'xy': 2}
+    plt.imshow(species[region].states3d[:].mean(plane2mean[plane]).T, interpolation='nearest', origin='upper')  #  extent=k[extracellular].extent('xy')
+    sb = scalebar.ScaleBar(1e-6)
+    sb.location='lower left'
+    ax = plt.gca()
+    ax.xaxis.set_visible(False)
+    ax.yaxis.set_visible(False)
+    plt.xlabel(plane[0])
+    plt.ylabel(plane[1])
+    ax.add_artist(sb)
+    plt.colorbar(label="$%s^+$ (mM)"%(species.name))
+
+    # show fig 
+    if showFig: _showFigure()
+    
+    return fig

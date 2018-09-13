c04a65c71805c7bd7c769f795206392de90afaaa
facu.r <bruseghini.f@gmail.com>
2018-09-13 16:02:16 -0300

    sync with development

--- netpyne/utils.py
+++ netpyne/utils.py
@@ -502,7 +502,7 @@ def importConnFromExcel (fileName, sheetName):
         
 def ValidateFunction(strFunc, netParamsVars):
     ''' returns True if "strFunc" can be evaluated'''
-    
+    from math import exp, log, sqrt, int, sin, cos, tan, asin, acos, atan, sinh, cosh, tangh, pi, e 
     rand = h.Random()
     stringFuncRandMethods = ['binomial', 'discunif', 'erlang', 'geometric', 'hypergeo', 
         'lognormal', 'negexp', 'normal', 'poisson', 'uniform', 'weibull']
@@ -604,3 +604,44 @@ def createScript(fname, netParams, simConfig):
 
     except:
         print('error saving file: %s' %(sys.exc_info()[1]))
+
+def bashTemplate(template):
+    ''' return the bash commands required by template for batch simulation'''
+    
+    if template=='mpi_direct':
+        return """#!/bin/bash 
+%s
+cd %s
+%s
+        """
+    elif template=='hpc_slurm':
+        return """#!/bin/bash 
+#SBATCH --job-name=%s
+#SBATCH -A %s
+#SBATCH -t %s
+#SBATCH --nodes=%d
+#SBATCH --ntasks-per-node=%d
+#SBATCH -o %s.run
+#SBATCH -e %s.err
+#SBATCH --mail-user=%s
+#SBATCH --mail-type=end
+%s
+%s
+source ~/.bashrc
+cd %s
+%s
+wait
+        """
+    elif template=='hpc_torque':
+        return """#!/bin/bash 
+#PBS -N %s
+#PBS -l walltime=%s
+#PBS -q %s
+#PBS -l %s
+#PBS -o %s.run
+#PBS -e %s.err
+%s
+cd $PBS_O_WORKDIR
+echo $PBS_O_WORKDIR
+%s
+        """

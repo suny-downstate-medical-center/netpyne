b0dd45ba600d2f6a32ed59637e1f1ba22ee9b20f
salvadord <salvadordura@gmail.com>
2018-09-14 18:08:45 -0400

    refactoring simFuncs

---
+++ netpyne/sim/gather.py
@@ -0,0 +1,340 @@
+"""
+sim/gather.py
+
+Functions related to gathering data from nodes after the simulation
+
+Contributors: salvadordura@gmail.com
+"""
+
+import numpy as np
+from ..specs import Dict, ODict
+
+
+#------------------------------------------------------------------------------
+# Gather data from nodes
+#------------------------------------------------------------------------------
+def gatherData (gatherLFP = True):
+    from .. import sim
+
+    sim.timing('start', 'gatherTime')
+    ## Pack data from all hosts
+    if sim.rank==0:
+        print('\nGathering data...')
+
+    # flag to avoid saving sections data for each cell (saves gather time and space; cannot inspect cell secs or re-simulate)
+    if not sim.cfg.saveCellSecs:
+        for cell in sim.net.cells:
+            cell.secs = None
+            cell.secLists = None
+
+    # flag to avoid saving conns data for each cell (saves gather time and space; cannot inspect cell conns or re-simulate)
+    if not sim.cfg.saveCellConns:
+        for cell in sim.net.cells:
+            cell.conns = []
+
+    # Store conns in a compact list format instead of a long dict format (cfg.compactConnFormat contains list of keys to include)
+    elif sim.cfg.compactConnFormat:
+        sim.compactConnFormat()
+            
+    # remove data structures used to calculate LFP 
+    if gatherLFP and sim.cfg.recordLFP and hasattr(sim.net, 'compartCells') and sim.cfg.createNEURONObj:
+        for cell in sim.net.compartCells:
+            try:
+                del cell.imembVec
+                del cell.imembPtr
+                del cell._segCoords
+            except:
+                pass
+        for pop in sim.net.pops.values():
+            try:
+                del pop._morphSegCoords
+            except:
+                pass
+
+    simDataVecs = ['spkt','spkid','stims']+sim.cfg.recordTraces.keys()
+    singleNodeVecs = ['t']
+    if sim.nhosts > 1:  # only gather if >1 nodes
+        netPopsCellGids = {popLabel: list(pop.cellGids) for popLabel,pop in sim.net.pops.iteritems()}
+
+        # gather only sim data
+        if getattr(sim.cfg, 'gatherOnlySimData', False):
+            nodeData = {'simData': sim.simData}
+            data = [None]*sim.nhosts
+            data[0] = {}
+            for k,v in nodeData.iteritems():
+                data[0][k] = v
+            gather = sim.pc.py_alltoall(data)
+            sim.pc.barrier()
+
+            if sim.rank == 0: # simData
+                print '  Gathering only sim data...'
+                sim.allSimData = Dict()
+                for k in gather[0]['simData'].keys():  # initialize all keys of allSimData dict
+                    if gatherLFP and k == 'LFP':
+                        sim.allSimData[k] = np.zeros((gather[0]['simData']['LFP'].shape))
+                    else:
+                        sim.allSimData[k] = {}
+
+                for key in singleNodeVecs: # store single node vectors (eg. 't')
+                    sim.allSimData[key] = list(nodeData['simData'][key])
+
+                # fill in allSimData taking into account if data is dict of h.Vector (code needs improvement to be more generic)
+                for node in gather:  # concatenate data from each node
+                    for key,val in node['simData'].iteritems():  # update simData dics of dics of h.Vector
+                        if key in simDataVecs:          # simData dicts that contain Vectors
+                            if isinstance(val, dict):
+                                for cell,val2 in val.iteritems():
+                                    if isinstance(val2,dict):
+                                        sim.allSimData[key].update(Dict({cell:Dict()}))
+                                        for stim,val3 in val2.iteritems():
+                                            sim.allSimData[key][cell].update({stim:list(val3)}) # udpate simData dicts which are dicts of dicts of Vectors (eg. ['stim']['cell_1']['backgrounsd']=h.Vector)
+                                    else:
+                                        sim.allSimData[key].update({cell:list(val2)})  # udpate simData dicts which are dicts of Vectors (eg. ['v']['cell_1']=h.Vector)
+                            else:
+                                sim.allSimData[key] = list(sim.allSimData[key])+list(val) # udpate simData dicts which are Vectors
+                        elif gatherLFP and key == 'LFP':
+                            sim.allSimData[key] += np.array(val)
+                        elif key not in singleNodeVecs:
+                            sim.allSimData[key].update(val)           # update simData dicts which are not Vectors
+
+                if len(sim.allSimData['spkt']) > 0:
+                    sim.allSimData['spkt'], sim.allSimData['spkid'] = zip(*sorted(zip(sim.allSimData['spkt'], sim.allSimData['spkid']))) # sort spks
+
+                sim.net.allPops = ODict() # pops
+                for popLabel,pop in sim.net.pops.iteritems(): sim.net.allPops[popLabel] = pop.__getstate__() # can't use dict comprehension for OrderedDict
+
+                sim.net.allCells = [c.__dict__ for c in sim.net.cells]
+
+        # gather cells, pops and sim data
+        else:
+            nodeData = {'netCells': [c.__getstate__() for c in sim.net.cells], 'netPopsCellGids': netPopsCellGids, 'simData': sim.simData}
+            data = [None]*sim.nhosts
+            data[0] = {}
+            for k,v in nodeData.iteritems():
+                data[0][k] = v
+            
+            #print data
+            gather = sim.pc.py_alltoall(data)
+            sim.pc.barrier()
+            if sim.rank == 0:
+                allCells = []
+                allPops = ODict()
+                for popLabel,pop in sim.net.pops.iteritems(): allPops[popLabel] = pop.__getstate__() # can't use dict comprehension for OrderedDict
+                allPopsCellGids = {popLabel: [] for popLabel in netPopsCellGids}
+                sim.allSimData = Dict()
+
+                for k in gather[0]['simData'].keys():  # initialize all keys of allSimData dict
+                    if gatherLFP and k == 'LFP':
+                        sim.allSimData[k] = np.zeros((gather[0]['simData']['LFP'].shape))
+                    else:
+                        sim.allSimData[k] = {}
+
+                for key in singleNodeVecs:  # store single node vectors (eg. 't')
+                    sim.allSimData[key] = list(nodeData['simData'][key])
+
+                # fill in allSimData taking into account if data is dict of h.Vector (code needs improvement to be more generic)
+                for node in gather:  # concatenate data from each node
+                    allCells.extend(node['netCells'])  # extend allCells list
+                    for popLabel,popCellGids in node['netPopsCellGids'].iteritems():
+                        allPopsCellGids[popLabel].extend(popCellGids)
+
+                    for key,val in node['simData'].iteritems():  # update simData dics of dics of h.Vector
+                        if key in simDataVecs:          # simData dicts that contain Vectors
+                            if isinstance(val,dict):
+                                for cell,val2 in val.iteritems():
+                                    if isinstance(val2,dict):
+                                        sim.allSimData[key].update(Dict({cell:Dict()}))
+                                        for stim,val3 in val2.iteritems():
+                                            sim.allSimData[key][cell].update({stim:list(val3)}) # udpate simData dicts which are dicts of dicts of Vectors (eg. ['stim']['cell_1']['backgrounsd']=h.Vector)
+                                    else:
+                                        sim.allSimData[key].update({cell:list(val2)})  # udpate simData dicts which are dicts of Vectors (eg. ['v']['cell_1']=h.Vector)
+                            else:
+                                sim.allSimData[key] = list(sim.allSimData[key])+list(val) # udpate simData dicts which are Vectors
+                        elif gatherLFP and key == 'LFP':
+                            sim.allSimData[key] += np.array(val)
+                        elif key not in singleNodeVecs:
+                            sim.allSimData[key].update(val)           # update simData dicts which are not Vectors
+
+                if len(sim.allSimData['spkt']) > 0:
+                    sim.allSimData['spkt'], sim.allSimData['spkid'] = zip(*sorted(zip(sim.allSimData['spkt'], sim.allSimData['spkid']))) # sort spks
+
+                sim.net.allCells =  sorted(allCells, key=lambda k: k['gid'])
+
+                for popLabel,pop in allPops.iteritems():
+                    pop['cellGids'] = sorted(allPopsCellGids[popLabel])
+                sim.net.allPops = allPops
+
+
+        # clean to avoid mem leaks
+        for node in gather:
+            if node:
+                node.clear()
+                del node
+        for item in data:
+            if item:
+                item.clear()
+                del item
+
+    else:  # if single node, save data in same format as for multiple nodes for consistency
+        if sim.cfg.createNEURONObj:
+            sim.net.allCells = [Dict(c.__getstate__()) for c in sim.net.cells]
+        else:
+            sim.net.allCells = [c.__dict__ for c in sim.net.cells]
+        sim.net.allPops = ODict()
+        for popLabel,pop in sim.net.pops.iteritems(): sim.net.allPops[popLabel] = pop.__getstate__() # can't use dict comprehension for OrderedDict
+        sim.allSimData = Dict()
+        for k in sim.simData.keys():  # initialize all keys of allSimData dict
+            sim.allSimData[k] = Dict()
+        for key,val in sim.simData.iteritems():  # update simData dics of dics of h.Vector
+                if key in simDataVecs+singleNodeVecs:          # simData dicts that contain Vectors
+                    if isinstance(val,dict):
+                        for cell,val2 in val.iteritems():
+                            if isinstance(val2,dict):
+                                sim.allSimData[key].update(Dict({cell:Dict()}))
+                                for stim,val3 in val2.iteritems():
+                                    sim.allSimData[key][cell].update({stim:list(val3)}) # udpate simData dicts which are dicts of dicts of Vectors (eg. ['stim']['cell_1']['backgrounsd']=h.Vector)
+                            else:
+                                sim.allSimData[key].update({cell:list(val2)})  # udpate simData dicts which are dicts of Vectors (eg. ['v']['cell_1']=h.Vector)
+                    else:
+                        sim.allSimData[key] = list(sim.allSimData[key])+list(val) # udpate simData dicts which are Vectors
+                else:
+                    sim.allSimData[key] = val           # update simData dicts which are not Vectors
+
+    ## Print statistics
+    sim.pc.barrier()
+    if sim.rank == 0:
+        sim.timing('stop', 'gatherTime')
+        if sim.cfg.timing: print('  Done; gather time = %0.2f s.' % sim.timingData['gatherTime'])
+
+        print('\nAnalyzing...')
+        sim.totalSpikes = len(sim.allSimData['spkt'])
+        sim.totalSynapses = sum([len(cell['conns']) for cell in sim.net.allCells])
+        if sim.cfg.createPyStruct:
+            if sim.cfg.compactConnFormat:
+                preGidIndex = sim.cfg.compactConnFormat.index('preGid') if 'preGid' in sim.cfg.compactConnFormat else 0
+                sim.totalConnections = sum([len(set([conn[preGidIndex] for conn in cell['conns']])) for cell in sim.net.allCells])
+            else:
+                sim.totalConnections = sum([len(set([conn['preGid'] for conn in cell['conns']])) for cell in sim.net.allCells])
+        else:
+            sim.totalConnections = sim.totalSynapses
+        sim.numCells = len(sim.net.allCells)
+
+        if sim.totalSpikes > 0:
+            sim.firingRate = float(sim.totalSpikes)/sim.numCells/sim.cfg.duration*1e3 # Calculate firing rate
+        else:
+            sim.firingRate = 0
+        if sim.numCells > 0:
+            sim.connsPerCell = sim.totalConnections/float(sim.numCells) # Calculate the number of connections per cell
+            sim.synsPerCell = sim.totalSynapses/float(sim.numCells) # Calculate the number of connections per cell
+        else:
+            sim.connsPerCell = 0
+            sim.synsPerCell = 0
+
+        print('  Cells: %i' % (sim.numCells) )
+        print('  Connections: %i (%0.2f per cell)' % (sim.totalConnections, sim.connsPerCell))
+        if sim.totalSynapses != sim.totalConnections:
+            print('  Synaptic contacts: %i (%0.2f per cell)' % (sim.totalSynapses, sim.synsPerCell))
+        if 'runTime' in sim.timingData:
+            print('  Spikes: %i (%0.2f Hz)' % (sim.totalSpikes, sim.firingRate))
+            if sim.cfg.printPopAvgRates and not sim.cfg.gatherOnlySimData:
+                trange = sim.cfg.printPopAvgRates if isinstance(sim.cfg.printPopAvgRates,list) else None
+                sim.allSimData['popRates'] = sim.analysis.popAvgRates(trange=trange)
+            print('  Simulated time: %0.1f s; %i workers' % (sim.cfg.duration/1e3, sim.nhosts))
+            print('  Run time: %0.2f s' % (sim.timingData['runTime']))
+
+            sim.allSimData['avgRate'] = sim.firingRate  # save firing rate
+
+        return sim.allSimData
+
+
+#------------------------------------------------------------------------------
+# Gather tags from cells
+#------------------------------------------------------------------------------
+def _gatherAllCellTags ():
+    from .. import sim
+
+    data = [{cell.gid: cell.tags for cell in sim.net.cells}]*sim.nhosts  # send cells data to other nodes
+    gather = sim.pc.py_alltoall(data)  # collect cells data from other nodes (required to generate connections)
+    sim.pc.barrier()
+    allCellTags = {}
+    for dataNode in gather:
+        allCellTags.update(dataNode)
+
+    # clean to avoid mem leaks
+    for node in gather:
+        if node:
+            node.clear()
+            del node
+    for item in data:
+        if item:
+            item.clear()
+            del item
+
+    return allCellTags
+
+
+#------------------------------------------------------------------------------
+# Gather tags from cells
+#------------------------------------------------------------------------------
+def _gatherAllCellConnPreGids ():
+    from .. import sim
+
+    data = [{cell.gid: [conn['preGid'] for conn in cell.conns] for cell in sim.net.cells}]*sim.nhosts  # send cells data to other nodes
+    gather = sim.pc.py_alltoall(data)  # collect cells data from other nodes (required to generate connections)
+    sim.pc.barrier()
+    allCellConnPreGids = {}
+    for dataNode in gather:
+        allCellConnPreGids.update(dataNode)
+
+    # clean to avoid mem leaks
+    for node in gather:
+        if node:
+            node.clear()
+            del node
+    for item in data:
+        if item:
+            item.clear()
+            del item
+
+    return allCellConnPreGids
+
+
+#------------------------------------------------------------------------------
+# Gather data from nodes
+#------------------------------------------------------------------------------
+def _gatherCells ():
+    from .. import sim
+
+    ## Pack data from all hosts
+    if sim.rank==0:
+        print('\nUpdating sim.net.allCells...')
+
+    if sim.nhosts > 1:  # only gather if >1 nodes
+        nodeData = {'netCells': [c.__getstate__() for c in sim.net.cells]}
+        data = [None]*sim.nhosts
+        data[0] = {}
+        for k,v in nodeData.iteritems():
+            data[0][k] = v
+        gather = sim.pc.py_alltoall(data)
+        sim.pc.barrier()
+        if sim.rank == 0:
+            allCells = []
+
+            # fill in allSimData taking into account if data is dict of h.Vector (code needs improvement to be more generic)
+            for node in gather:  # concatenate data from each node
+                allCells.extend(node['netCells'])  # extend allCells list
+            sim.net.allCells =  sorted(allCells, key=lambda k: k['gid'])
+
+        # clean to avoid mem leaks
+        for node in gather:
+            if node:
+                node.clear()
+                del node
+        for item in data:
+            if item:
+                item.clear()
+                del item
+
+    else:  # if single node, save data in same format as for multiple nodes for consistency
+        sim.net.allCells = [c.__getstate__() for c in sim.net.cells]
+
---
+++ netpyne/sim/save.py
@@ -0,0 +1,216 @@
+"""
+sim/save.py
+
+Functions related to saving 
+
+Contributors: salvadordura@gmail.com
+"""
+
+from time import time
+from datetime import datetime
+import cPickle as pk
+from . import gather
+from . import utils
+
+#------------------------------------------------------------------------------
+# Save data
+#------------------------------------------------------------------------------
+def saveData (include = None, filename = None):
+    from .. import sim
+
+    if sim.rank == 0 and not getattr(sim.net, 'allCells', None): needGather = True
+    else: needGather = False
+    if needGather: gather.gatherData()
+
+    if filename: sim.cfg.filename = filename
+
+    if sim.rank == 0:
+        sim.timing('start', 'saveTime')
+        import os
+
+        # copy source files
+        if isinstance(sim.cfg.backupCfgFile, list) and len(sim.cfg.backupCfgFile) == 2:
+            simName = sim.cfg.simLabel if sim.cfg.simLabel else os.path.basename(sim.cfg.filename)
+            print('Copying cfg file %s ... ' % simName)
+            source = sim.cfg.backupCfgFile[0]
+            targetFolder = sim.cfg.backupCfgFile[1]
+            # make dir
+            try:
+                os.mkdir(targetFolder)
+            except OSError:
+                if not os.path.exists(targetFolder):
+                    print ' Could not create target folder: %s' % (targetFolder)
+            # copy file
+            targetFile = targetFolder + '/' + simName + '_cfg.py'
+            if os.path.exists(targetFile):
+                print ' Removing prior cfg file' , targetFile
+                os.system('rm ' + targetFile)
+            os.system('cp ' + source + ' ' + targetFile)
+
+
+        # create folder if missing
+        targetFolder = os.path.dirname(sim.cfg.filename)
+        if targetFolder and not os.path.exists(targetFolder):
+            try:
+                os.mkdir(targetFolder)
+            except OSError:
+                print ' Could not create target folder: %s' % (targetFolder)
+
+        # saving data
+        if not include: include = sim.cfg.saveDataInclude
+        dataSave = {}
+        net = {}
+
+        dataSave['netpyne_version'] = sim.version(show=False)
+        dataSave['netpyne_changeset'] = sim.gitChangeset(show=False)
+        
+        if getattr(sim.net.params, 'version', None): dataSave['netParams_version'] = sim.net.params.version
+        if 'netParams' in include: 
+            sim.net.params.__dict__.pop('_labelid', None)
+            net['params'] = utils.replaceFuncObj(sim.net.params.__dict__)
+        if 'net' in include: include.extend(['netPops', 'netCells'])
+        if 'netCells' in include: net['cells'] = sim.net.allCells
+        if 'netPops' in include: net['pops'] = sim.net.allPops
+        if net: dataSave['net'] = net
+        if 'simConfig' in include: dataSave['simConfig'] = sim.cfg.__dict__
+        if 'simData' in include: 
+            if 'LFP' in sim.allSimData: 
+                sim.allSimData['LFP'] = sim.allSimData['LFP'].tolist() 
+            dataSave['simData'] = sim.allSimData
+
+
+        if dataSave:
+            if sim.cfg.timestampFilename:
+                timestamp = time()
+                timestampStr = '-' + datetime.fromtimestamp(timestamp).strftime('%Y%m%d_%H%M%S')
+            else:
+                timestampStr = ''
+            
+            filePath = sim.cfg.filename + timestampStr
+            # Save to pickle file
+            if sim.cfg.savePickle:
+                import pickle
+                dataSave = utils.replaceDictODict(dataSave)
+                print('Saving output as %s ... ' % (filePath+'.pkl'))
+                with open(filePath+'.pkl', 'wb') as fileObj:
+                    pickle.dump(dataSave, fileObj)
+                print('Finished saving!')
+
+            # Save to dpk file
+            if sim.cfg.saveDpk:
+                import gzip
+                print('Saving output as %s ... ' % (filePath+'.dpk'))
+                #fn=filePath #.split('.')
+                gzip.open(filePath, 'wb').write(pk.dumps(dataSave)) # write compressed string
+                print('Finished saving!')
+
+            # Save to json file
+            if sim.cfg.saveJson:
+                import json
+                #dataSave = utils.replaceDictODict(dataSave)  # not required since json saves as dict
+                print('Saving output as %s ... ' % (filePath+'.json '))
+                with open(filePath+'.json', 'w') as fileObj:
+                    json.dump(dataSave, fileObj)
+                print('Finished saving!')
+
+            # Save to mat file
+            if sim.cfg.saveMat:
+                from scipy.io import savemat
+                print('Saving output as %s ... ' % (filePath+'.mat'))
+                savemat(filePath+'.mat', utils.tupleToList(utils.replaceNoneObj(dataSave)))  # replace None and {} with [] so can save in .mat format
+                print('Finished saving!')
+
+            # Save to HDF5 file (uses very inefficient hdf5storage module which supports dicts)
+            if sim.cfg.saveHDF5:
+                dataSaveUTF8 = utils._dict2utf8(utils.replaceNoneObj(dataSave)) # replace None and {} with [], and convert to utf
+                import hdf5storage
+                print('Saving output as %s... ' % (filePath+'.hdf5'))
+                hdf5storage.writes(dataSaveUTF8, filename=filePath+'.hdf5')
+                print('Finished saving!')
+
+            # Save to CSV file (currently only saves spikes)
+            if sim.cfg.saveCSV:
+                if 'simData' in dataSave:
+                    import csv
+                    print('Saving output as %s ... ' % (filePath+'.csv'))
+                    writer = csv.writer(open(filePath+'.csv', 'wb'))
+                    for dic in dataSave['simData']:
+                        for values in dic:
+                            writer.writerow(values)
+                    print('Finished saving!')
+
+            # Save to Dat file(s)
+            if sim.cfg.saveDat:
+                traces = sim.cfg.recordTraces
+                for ref in traces.keys():
+                    for cellid in sim.allSimData[ref].keys():
+                        dat_file_name = '%s_%s.dat'%(ref,cellid)
+                        dat_file = open(dat_file_name, 'w')
+                        trace = sim.allSimData[ref][cellid]
+                        print("Saving %i points of data on: %s:%s to %s"%(len(trace),ref,cellid,dat_file_name))
+                        for i in range(len(trace)):
+                            dat_file.write('%s\t%s\n'%((i*sim.cfg.dt/1000),trace[i]/1000))
+
+                print('Finished saving!')
+
+            # Save timing
+            if sim.cfg.timing:
+                sim.timing('stop', 'saveTime')
+                print('  Done; saving time = %0.2f s.' % sim.timingData['saveTime'])
+            if sim.cfg.timing and sim.cfg.saveTiming:
+                import pickle
+                with open('timing.pkl', 'wb') as file: pickle.dump(sim.timing, file)
+
+
+            # clean to avoid mem leaks
+            for key in dataSave.keys():
+                del dataSave[key]
+            del dataSave
+
+            # return full path
+            import os
+            return os.getcwd() + '/' + filePath
+
+        else:
+            print('Nothing to save')
+
+
+#------------------------------------------------------------------------------
+# Save distributed data using HDF5 (only conns for now)
+#------------------------------------------------------------------------------
+def distributedSaveHDF5():
+    from .. import sim
+    import h5py
+
+    if sim.rank == 0: sim.timing('start', 'saveTimeHDF5')
+
+    sim.compactConnFormat()
+    conns = [[cell.gid]+conn for cell in sim.net.cells for conn in cell.conns]
+    conns = sim.copyRemoveItemObj(conns, keystart='h', newval=[]) 
+    connFormat = ['postGid']+sim.cfg.compactConnFormat
+    with h5py.File(sim.cfg.filename+'.h5', 'w') as hf:
+        hf.create_dataset('conns', data = conns)
+        hf.create_dataset('connsFormat', data = connFormat)
+
+    if sim.rank == 0: sim.timing('stop', 'saveTimeHDF5')
+
+
+#------------------------------------------------------------------------------
+# Convet connections in long dict format to compact list format 
+#------------------------------------------------------------------------------
+def compactConnFormat():
+    from .. import sim
+
+    if type(sim.cfg.compactConnFormat) is not list:
+        if len(sim.net.params.stimTargetParams) > 0:  # if have stims, then require preLabel field
+            sim.cfg.compactConnFormat = ['preGid', 'preLabel', 'sec', 'loc', 'synMech', 'weight', 'delay']
+        else:
+            sim.cfg.compactConnFormat = ['preGid', 'sec', 'loc', 'synMech', 'weight', 'delay']
+    
+
+    connFormat = sim.cfg.compactConnFormat
+    for cell in sim.net.cells:
+        newConns = [[conn[param] for param in connFormat] for conn in cell.conns]
+        del cell.conns
+        cell.conns = newConns
+ 

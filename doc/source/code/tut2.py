a3f3137ff394ab14d4feef2bb41b7d72d78a9080
salvadord <salvadordura@gmail.com>
2018-09-18 20:00:53 -0400

    Fixed issue in probabilistic connectivity random number generation to ensure replicability in Python 2 and 3.

--- doc/source/code/tut2.py
+++ doc/source/code/tut2.py
@@ -36,7 +36,7 @@ simConfig = specs.SimConfig()		# object of class SimConfig to store simulation c
 
 simConfig.duration = 1*1e3 			# Duration of the simulation, in ms
 simConfig.dt = 0.025 				# Internal integration timestep to use
-simConfig.verbose = False  			# Show detailed messages
+simConfig.verbose = 1  			# Show detailed messages
 simConfig.recordTraces = {'V_soma':{'sec':'soma','loc':0.5,'var':'v'}}  # Dict with traces to record
 simConfig.recordStep = 0.1 			# Step size in ms to save data (eg. V traces, LFP, etc)
 simConfig.filename = 'model_output'  # Set file output name

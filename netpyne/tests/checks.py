c1639b719f22ac673a5195603376b700be162e83
salvadord <salvadordura@gmail.com>
2018-10-26 17:20:26 -0400

    updated checkOutput values for tuts and examples

--- netpyne/tests/checks.py
+++ netpyne/tests/checks.py
@@ -8,44 +8,44 @@ def checkOutput(modelName, verbose=False):
 		expectedAll = {'numSyns': {}, 'numSpikes': {}}
 
 		# tut2 expected output 
-		expectedAll['numSyns']['tut2'] = 254
-		expectedAll['numSpikes']['tut2'] = 944
+		expectedAll['numSyns']['tut2'] = 280
+		expectedAll['numSpikes']['tut2'] = 931
 
 		# tut3 expected output 
-		expectedAll['numSyns']['tut3'] = 254
-		expectedAll['numSpikes']['tut3'] = 538
+		expectedAll['numSyns']['tut3'] = 243
+		expectedAll['numSpikes']['tut3'] = 560
 
 		# tut4 expected output 
 		expectedAll['numSyns']['tut4'] = 73
-		expectedAll['numSpikes']['tut4'] = 1210
+		expectedAll['numSpikes']['tut4'] = 1197
 
 		# tut5 expected output 
-		expectedAll['numSyns']['tut5'] = 7040
-		expectedAll['numSpikes']['tut5'] = 4659
+		expectedAll['numSyns']['tut5'] = 7096
+		expectedAll['numSpikes']['tut5'] = 4879
 
 		# tut6 expected output 
 		expectedAll['numSyns']['tut6'] = 16
-		expectedAll['numSpikes']['tut6'] = 146
+		expectedAll['numSpikes']['tut6'] = 134
 
 		# tut7 expected output 
 		expectedAll['numSyns']['tut7'] = 2500
 		expectedAll['numSpikes']['tut7'] = 332
 
 		# tut_import expected output 
-		expectedAll['numSyns']['tut_import'] = 340
-		expectedAll['numSpikes']['tut_import'] = 3061  # check Traub cell mismatch
+		expectedAll['numSyns']['tut_import'] = 365
+		expectedAll['numSpikes']['tut_import'] = 3023  # check Traub cell mismatch
 
 		# HHTut expected output 
-		expectedAll['numSyns']['HHTut'] = 1839
+		expectedAll['numSyns']['HHTut'] = 1823
 		expectedAll['numSpikes']['HHTut'] = 2052
 
 		# HybridTut expected output 
-		expectedAll['numSyns']['HybridTut'] = 386
-		expectedAll['numSpikes']['HybridTut'] = 2766
+		expectedAll['numSyns']['HybridTut'] = 356
+		expectedAll['numSpikes']['HybridTut'] = 2561
 
 		# M1 expected output 
-		expectedAll['numSyns']['M1'] = 4836
-		expectedAll['numSpikes']['M1'] = 59755
+		expectedAll['numSyns']['M1'] = 4887
+		expectedAll['numSpikes']['M1'] = 76135
 
 		# PTcell expected output 
 		expectedAll['numSyns']['PTcell'] = 1

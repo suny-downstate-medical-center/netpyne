02e31ad8329633a3092e6c04f5aa098937bb76af
salvadord <salvadordura@gmail.com>
2018-10-30 13:21:49 -0400

    removed future from root __init__ to avoid setup.py error

--- netpyne/__init__.py
+++ netpyne/__init__.py
@@ -1,9 +1,3 @@
-from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
 
-from future import standard_library
-standard_library.install_aliases()
 __version__ = '0.7.9' 
 __gui__ = True  # global option to enable/disable graphics

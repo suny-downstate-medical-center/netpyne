a5c9ae2333aa4b923eaec33e9fcc6ce34ca6438e
Bill Lytton; cell 917 613 3991 <neuro014@gmail.com>
2020-05-06 13:09:04 -0400

    remove import __future__ stuff from conn.py; none of these routines are not called in this module

--- netpyne/network/conn.py
+++ netpyne/network/conn.py
@@ -1,4 +1,3 @@
-d
 """
 network/conn.py 
 
@@ -6,11 +5,6 @@ Network class methods to create connections
 
 Contributors: salvadordura@gmail.com
 """
-from __future__ import print_function
-from __future__ import division
-from __future__ import unicode_literals
-from __future__ import absolute_import
-
 from builtins import dict
 from builtins import range
 

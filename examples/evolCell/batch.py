8ac842c273ef0ddc427fce1ba61fd519eb8593ed
salvadord <salvadordura@gmail.com>
2020-05-11 12:43:41 -0400

    set up for NGF cells

--- examples/evolCell/batch.py
+++ examples/evolCell/batch.py
@@ -127,38 +127,38 @@ def evolCellNGF():
     params = specs.ODict()
 
     params[('tune', 'soma', 'Ra')] = [14., 14.]#*0.5, 14.*1.5] 
-    # params[('tune', 'soma', 'cm')] = [1.5*0.5, 1.5*1.5] 
-    # params[('tune', 'soma', 'ch_CavL', 'gmax')] = [0.056108352*0.5, 0.056108352*1.5] 
-    # params[('tune', 'soma', 'ch_CavN', 'gmax')] = [0.00058169587*0.5, 0.00058169587*1.5] 
-    # params[('tune', 'soma', 'ch_KCaS', 'gmax')] = [0.006 * 0.5, 0.006 * 1.5]
-    # params[('tune', 'soma', 'ch_Kdrfastngf', 'gmax')] = [0.09*0.5, 0.09*1.5] 
-    # params[('tune', 'soma', 'ch_KvAngf', 'gmax')] = [0.052*0.5, 0.052*1.5] 
-    # params[('tune', 'soma', 'ch_KvAngf', 'gml')] = [1.0*0.5, 1.0*1.5] 
-    # params[('tune', 'soma', 'ch_KvAngf', 'gmn')] = [0.6*0.5, 0.6*1.5] 
-    # params[('tune', 'soma', 'ch_KvCaB', 'gmax')] = [1.0235317e-06*0.5, 1.0235317e-06*1.5] 
-    # params[('tune', 'soma', 'ch_Navngf', 'gmax')] = [0.1*0.5, 0.1*1.5] 
-    # params[('tune', 'soma', 'hd', 'ehd')] = [-30*1.5, -30*0.5] 
-    # params[('tune', 'soma', 'hd', 'elk')] = [-70*1.5, -70*0.5] 
-    # params[('tune', 'soma', 'hd', 'gbar')] = [1e-05*0.5, 1e-05*1.5] 
-    # params[('tune', 'soma', 'hd', 'vhalfl')] = [-90*1.5, -90*0.5] 
-    # # params[('tune', 'soma', 'iconc_Ca', 'caiinf')] = [5e-06*0.5, 5e-06*1.5]   # convergence issues
-    # # params[('tune', 'soma', 'iconc_Ca', 'catau')] = [-10*1.5, -10*0.5] 
-    # params[('tune', 'soma', 'pas', 'e')] = [-85*1.5, -85*0.5]
-    # params[('tune', 'soma', 'pas', 'g')] = [5e-7*0.5, 5e-7*1.5]  
+    params[('tune', 'soma', 'cm')] = [1.5*0.5, 1.5*1.5] 
+    params[('tune', 'soma', 'ch_CavL', 'gmax')] = [0.056108352*0.5, 0.056108352*1.5] 
+    params[('tune', 'soma', 'ch_CavN', 'gmax')] = [0.00058169587*0.5, 0.00058169587*1.5] 
+    params[('tune', 'soma', 'ch_KCaS', 'gmax')] = [0.006 * 0.5, 0.006 * 1.5]
+    params[('tune', 'soma', 'ch_Kdrfastngf', 'gmax')] = [0.09*0.5, 0.09*1.5] 
+    params[('tune', 'soma', 'ch_KvAngf', 'gmax')] = [0.052*0.5, 0.052*1.5] 
+    params[('tune', 'soma', 'ch_KvAngf', 'gml')] = [1.0*0.5, 1.0*1.5] 
+    params[('tune', 'soma', 'ch_KvAngf', 'gmn')] = [0.6*0.5, 0.6*1.5] 
+    params[('tune', 'soma', 'ch_KvCaB', 'gmax')] = [1.0235317e-06*0.5, 1.0235317e-06*1.5] 
+    params[('tune', 'soma', 'ch_Navngf', 'gmax')] = [0.1*0.5, 0.1*1.5] 
+    params[('tune', 'soma', 'hd', 'ehd')] = [-30*1.5, -30*0.5] 
+    params[('tune', 'soma', 'hd', 'elk')] = [-70*1.5, -70*0.5] 
+    params[('tune', 'soma', 'hd', 'gbar')] = [1e-05*0.5, 1e-05*1.5] 
+    params[('tune', 'soma', 'hd', 'vhalfl')] = [-90*1.5, -90*0.5] 
+    # params[('tune', 'soma', 'iconc_Ca', 'caiinf')] = [5e-06*0.5, 5e-06*1.5]   # convergence issues
+    # params[('tune', 'soma', 'iconc_Ca', 'catau')] = [-10*1.5, -10*0.5] 
+    params[('tune', 'soma', 'pas', 'e')] = [-85*1.5, -85*0.5]
+    params[('tune', 'soma', 'pas', 'g')] = [5e-7*0.5, 5e-7*1.5]  
 
-    # params[('tune', 'dend', 'Ra')] = [14*0.5, 14*1.5] 
-    # params[('tune', 'dend', 'cm')] = [1.5*0.5, 1.5*1.5] 
-    # params[('tune', 'dend', 'ch_Kdrfastngf', 'gmax')] = [0.03*0.5, 0.03*1.5] 
-    # params[('tune', 'dend', 'ch_Navngf', 'gmax')] = [3.7860265*0.5, 3.7860265*1.5] 
-    # params[('tune', 'dend', 'pas', 'e')] = [-67*1.5, -67*0.5] 
-    # params[('tune', 'dend', 'pas', 'g')] = [0.0003*0.5, 0.0003*1.5] 
+    params[('tune', 'dend', 'Ra')] = [14*0.5, 14*1.5] 
+    params[('tune', 'dend', 'cm')] = [1.5*0.5, 1.5*1.5] 
+    params[('tune', 'dend', 'ch_Kdrfastngf', 'gmax')] = [0.03*0.5, 0.03*1.5] 
+    params[('tune', 'dend', 'ch_Navngf', 'gmax')] = [3.7860265*0.5, 3.7860265*1.5] 
+    params[('tune', 'dend', 'pas', 'e')] = [-67*1.5, -67*0.5] 
+    params[('tune', 'dend', 'pas', 'g')] = [0.0003*0.5, 0.0003*1.5] 
 
 
     # current injection params
     interval = 10000
     dur = 500  # ms
     durSteady = 200  # ms
-    amps = list(np.arange(0.04+0.075, 4.0, 0.05))# 0.121+0.075, 0.01))  # amplitudes
+    amps = list(np.arange(0.04+0.075, 0.121+0.075, 0.01))  # amplitudes
     times = list(np.arange(1000, (dur+interval) * len(amps), dur+interval))  # start times
     targetRatesOnset = [43., 52., 68., 80., 96., 110., 119., 131., 139.]
     targetRatesSteady = [22., 24., 27., 30., 33., 35., 37., 39., 41.]
@@ -185,8 +185,8 @@ def evolCellNGF():
     initCfg[('analysis', 'plotfI', 'dur')] = dur
     initCfg[('analysis', 'plotfI', 'durSteady')] = durSteady
     initCfg[('analysis', 'plotfI', 'targetRates')] = [] #
-    initCfg[('analysis', 'plotfI', 'targetRatesOnset')] = [] #targetRatesOnset
-    initCfg[('analysis', 'plotfI', 'targetRatesSteady')] = [] #targetRatesSteady
+    initCfg[('analysis', 'plotfI', 'targetRatesOnset')] = targetRatesOnset
+    initCfg[('analysis', 'plotfI', 'targetRatesSteady')] = targetRatesSteady
     
     for k, v in params.items():
         initCfg[k] = v[0]  # initialize params in cfg so they can be modified    
@@ -236,12 +236,12 @@ def evolCellNGF():
         'evolAlgorithm': 'custom',
         'fitnessFunc': fitnessFunc, # fitness expression (should read simData)
         'fitnessFuncArgs': fitnessFuncArgs,
-        'pop_size': 1,
+        'pop_size': 9,
         'num_elites': 1, # keep this number of parents for next generation if they are fitter than children
         'mutation_rate': 0.4,
         'crossover': 0.5,
         'maximize': False, # maximize fitness function?
-        'max_generations': 1,
+        'max_generations': 10,
         'time_sleep': 5, # wait this time before checking again if sim is completed (for each generation)
         'maxiter_wait': 20, # max number of times to check if sim is completed (for each generation)
         'defaultFitness': 1000 # set fitness value in case simulation time is over

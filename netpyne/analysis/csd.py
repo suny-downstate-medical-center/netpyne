33bfb46fc714bb1d711bdb66a9bf3eb8aba0e5a7
ericagriffith <ericaygriffith@gmail.com>
2020-05-21 11:10:57 -0400

    adding csd.py script to csd branch of local clone of ericaygriffith fork

---
+++ netpyne/analysis/csd.py
@@ -0,0 +1,12 @@
+#### THIS IS A PRACTICE DRAFT SCRIPT FOR CSD.PY -- EVENTUALLY WILL GO INTO NETPYNE DEV BRANCH IN ANALYSIS 
+
+"""
+Functions to plot and extract CSD info from LFP data
+"""
+
+### THIS SET OF IMPORTS FROM FUTURE IS IN lfp.py, traces.py, and spikes.py
+from __future__ import print_function
+from __future__ import division
+from __future__ import unicode_literals
+from __future__ import absolute_import
+

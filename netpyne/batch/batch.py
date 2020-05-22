4d120f3acb1847c747e90fa70bdcebf4476f9f40
Salvador Dura-Bernal <salvadordura@gmail.com>
2020-05-22 11:31:25 -0400

    removed delete folder (sometimes want to complete prev batch);

--- netpyne/batch/batch.py
+++ netpyne/batch/batch.py
@@ -69,15 +69,6 @@ def runJob(script, cfgSavePath, netParamsSavePath):
 # -------------------------------------------------------------------------------
 def createFolder(folder):
     import os
-    if os.path.exists(folder):
-        if pc.id()==0:
-            try:
-                import shutil
-                shutil.rmtree(folder)
-                # shutil.rmtree(folder)
-            except OSError:
-                print(' Could not delete %s' %(folder))
-        
     if not os.path.exists(folder):
         try:
             os.mkdir(folder)

1c4a883986f8e61339a6bd231761f0e73e769ac3
salvadord <salvadordura@gmail.com>
2020-07-07 18:18:01 -0400

    restored rxd examples

--- examples/rxd_buffering/buffering.py
+++ examples/rxd_buffering/buffering.py
@@ -2,7 +2,6 @@ from neuron import h
 from netpyne import specs, sim
 
 
-
 #------------------------------------------------------------------------------
 # NETWORK PARAMETERS
 #------------------------------------------------------------------------------
@@ -23,18 +22,13 @@ netParams.rxdParams['regions'] = {'cyt': {'cells': ['all'], 'secs': 'soma', 'nrn
 species = {}
 species['ca'] = {'regions': ['cyt'], 'charge': 2, 'initial': 1e-4}
 species['buf'] = {'regions': ['cyt'], 'initial': 1e-4}
-species['kbuf'] = {'regions': ['cyt'], 'initial': 0}
-species['k'] = {'regions': ['cyt'], 'd': 1.0, 'initial': 0.1, 'charge': 1} 
-
+species['cabuf'] = {'regions': ['cyt'], 'initial': 0}
 netParams.rxdParams['species'] = species
 
 ### reactions
 kf = 1e6
 kb = 1e-2
-#netParams.rxdParams['reactions'] = {'buffering': {'reactant': '2 * ca + buf', 'product': 'cabuf', 'rate_f': kf, 'rate_b': kb}}
-reactions = {}
-reactions['buffering'] = {'reactant': 'k+buf', 'product': 'kbuf', 'rate_f': '0.0008/(1+rxdmath.pow(2.71828,((7.5-k)/1.15)))','rate_b': 0.0008}
-netParams.rxdParams['reactions'] = reactions
+netParams.rxdParams['reactions'] = {'buffering': {'reactant': '2 * ca + buf', 'product': 'cabuf', 'rate_f': kf, 'rate_b': kb}}
 
 ### rates
 netParams.rxdParams['rates'] = {'degradation': {'species': 'buf', 'rate': '-1e-3 * buf'}}
@@ -48,7 +42,7 @@ netParams.rxdParams['rates'] = {'degradation': {'species': 'buf', 'rate': '-1e-3
 # Run parameters
 cfg = specs.SimConfig()       # object of class cfg to store simulation configuration
 cfg.duration = 500            # Duration of the simulation, in ms
-cfg.recordTraces = {'k': {'sec': 'soma', 'loc': 0.5, 'var': 'cai'},
+cfg.recordTraces = {'ca': {'sec': 'soma', 'loc': 0.5, 'var': 'cai'},
                     'buf': {'sec': 'soma', 'loc': 0.5, 'var': 'bufi'},
                     'cabuf': {'sec': 'soma', 'loc': 0.5, 'var': 'cabufi'}}
 
@@ -60,6 +54,7 @@ cfg.analysis['plotTraces'] = {'include': ['cell'], 'ylim':[0, 0.0001], 'overlay'
 # RUN MODEL
 #------------------------------------------------------------------------------
 sim.create(netParams,cfg)
+h.finitialize()
 sim.simulate()
 sim.analyze()
 

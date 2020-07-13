efefbdd1cf3ea2c753d41f60a67051c5ca17250c
graham <joe.w.graham@gmail.com>
2020-07-13 12:03:03 -0700

    Minor improvements to netpyne/__init__.py

--- netpyne/__init__.py
+++ netpyne/__init__.py
@@ -1,5 +1,5 @@
 """
-NetPyNE (Networks using Python and NEURON) is a Python package to facilitate the development, simulation, parallelization, and analysis of biological neuronal networks using the NEURON simulator.
+NetPyNE (Networks using Python and NEURON) is a Python package to facilitate the development, simulation, parallelization, analysis, and optimization of biological neuronal networks using the NEURON simulator.
 
 NetPyNE consists of a number of sub-packages and modules.
 """

b57879bd90e4493eb9a81b2d06fe3d92c6f97f0b
Joe Graham <joe.w.graham@gmail.com>
2020-07-24 09:10:22 -0700

    https://github.com/joewgraham/netpyne/issues/54

--- netpyne/support/__init__.py
+++ netpyne/support/__init__.py
@@ -1,5 +1,7 @@
 """
-Package for/to <short description of `netpyne.support`>
+Package containing external modules or packages required by NetPyNE
+
+This subpackage contains external modules or packages required by NetPyNE which are either not available via pip or have been slightly modified.
 
 """
 

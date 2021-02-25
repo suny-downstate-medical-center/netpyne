8f331859170d5051e01ff37d4c9ca7c871a40d41
salvadord <salvadordura@gmail.com>
2021-02-24 23:04:55 -0500

    fixed iplotDipole to accept plotting external data

--- netpyne/analysis/interactive.py
+++ netpyne/analysis/interactive.py
@@ -372,8 +372,8 @@ def iplotRaster(include=['allCells'], timeRange=None, maxSpikes=1e8, orderBy='gi
 # -------------------------------------------------------------------------------------------------------------------
 ## Plot interactive dipole
 # -------------------------------------------------------------------------------------------------------------------
-@exception
-def iplotDipole(expData={'label': 'Experiment', 'x':[], 'y':[]}, showFig=False, **kwargs):
+#@exception
+def iplotDipole(expData={'label': 'Experiment', 'x':[], 'y':[]}, dpl=None, t=None,  showFig=False, **kwargs):
     """
     Function for/to <short description of `netpyne.analysis.interactive.iplotDipole`>
 
@@ -479,8 +479,17 @@ def iplotDipole(expData={'label': 'Experiment', 'x':[], 'y':[]}, showFig=False, 
         return convolve(x,win,'same')
 
     # baseline renormalize
-    dpl = baseline_renormalize()
+    if dpl:
+        dpl = {k: np.array(v) for k, v in dpl.items()}
+    else:
+        dpl = baseline_renormalize()
 
+    if t is not None and len(t)>0:
+        t=np.array(t)
+    else:
+        t=sim.simData['t']
+
+        
     # convert units from fAm to nAm, rescale and smooth
     for key in dpl.keys():
         dpl[key] *= 1e-6 * sim.cfg.hnn_params['dipole_scalefctr']
@@ -492,11 +501,11 @@ def iplotDipole(expData={'label': 'Experiment', 'x':[], 'y':[]}, showFig=False, 
         dpl[key][0] = 0.0
 
     # plot exp data
-    fig.line(expData['x'], expData['y'], color='black', legend=expData['label'])
+    #fig.line(expData['x'], expData['y'], color='black', legend=expData['label'])
 
     # plot recorded dipole data
     for i,(k,v) in enumerate(dpl.items()):
-        fig.line(sim.simData['t'], v, legend=k, color=colors[i], line_width=2.0)
+        fig.line(t, v, legend=k, color=colors[i], line_width=2.0)
 
     fig.legend.location = "top_right"
     fig.legend.click_policy = "hide"
@@ -507,7 +516,7 @@ def iplotDipole(expData={'label': 'Experiment', 'x':[], 'y':[]}, showFig=False, 
     if showFig:
         show(fig)
 
-    return html
+    return html, dpl
 
 
 # -------------------------------------------------------------------------------------------------------------------

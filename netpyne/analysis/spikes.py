9a6ec26847bfd3ffa0fa48ae08d8f6fc48168536
graham <joe.w.graham@gmail.com>
2021-03-29 20:42:23 -0700

    Generalized spike analysis for plotRaster and plotSpikeHist

--- netpyne/analysis/spikes.py
+++ netpyne/analysis/spikes.py
@@ -32,7 +32,7 @@ from .utils import saveData as saveFigData
 from ..support.scalebar import add_scalebar
 
 
-#@exception
+@exception
 def prepareSpikeData(include=['allCells'], sim=None, timeRange=None, maxSpikes=1e8, orderBy='gid', popRates=True, saveData=False, fileName=None, fileDesc=None, fileType=None, fileDir=None, **kwargs):
     """
     Function to prepare data for creating a raster plot, a spike histogram plot, ...

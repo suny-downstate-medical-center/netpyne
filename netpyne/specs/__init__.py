3e65bb6fc4793ee97d7200dbd82edf54e72ab535
tusharongith <50165840+tusharongith@users.noreply.github.com>
2021-04-08 13:55:09 +0530

    Improved code formatting

--- netpyne/specs/__init__.py
+++ netpyne/specs/__init__.py
@@ -3,13 +3,13 @@ Package of classes to define high-level model specifications
 
 """
 
-from __future__ import unicode_literals         #Importing unicode_literals from __future__
-from __future__ import print_function           #Importing print_function from __future__
-from __future__ import division                 #Importing division from __future__
-from __future__ import absolute_import          #Importing absolute_import from __future__
+from __future__ import unicode_literals        #Importing unicode_literals from __future__
+from __future__ import print_function          #Importing print_function from __future__
+from __future__ import division                #Importing division from __future__
+from __future__ import absolute_import         #Importing absolute_import from __future__
 
-from future import standard_library             #Importing standard_library from future
+from future import standard_library            #Importing standard_library from future
 standard_library.install_aliases()
-from .dicts import Dict, ODict                  #Importing Dict, ODict from .dicts
-from .netParams import NetParams, CellParams    #Importing NetParams, CellParams from .netParams
-from .simConfig import SimConfig                #Importing SimConfig from .simConfig
+from .dicts import Dict, ODict                 #Importing Dict, ODict from .dicts
+from .netParams import NetParams, CellParams   #Importing NetParams, CellParams from .netParams
+from .simConfig import SimConfig               #Importing SimConfig from .simConfig

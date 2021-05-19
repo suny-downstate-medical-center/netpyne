b8b4842172dc3252f91d7e42195bc92f83a822b9
craig <kelley92@bu.edu>
2021-05-19 15:33:31 -0400

    no filename

--- examples/batch_rxd_net/cfg.py
+++ examples/batch_rxd_net/cfg.py
@@ -7,10 +7,7 @@ from netpyne import specs
 #------------------------------------------------------------------------------
 # parameters
 cfg = specs.SimConfig()       # object of class cfg to store simulation configuration
-## Change ip3_init from 0 to 0.1 to observe multiscale effect:
-## high ip3 -> ER Ca released to Cyt -> kBK channels open -> less firing
-cfg.ip3_init = 0.0
-cfg.gip3r = 12040 * 100
+
 
 # Run parameters
 cfg.duration = 1.0*1e3        # Duration of the simulation, in ms
@@ -18,9 +15,12 @@ cfg.hParams['v_init'] = -65   # set v_init to -65 mV
 cfg.dt = 0.1                  # Internal integration timestep to use
 cfg.verbose = False            # Show detailed messages
 cfg.recordStep = 1             # Step size in ms to save data (eg. V traces, LFP, etc)
-cfg.filename = 'data/batchRxd/rxd_net_' + str(cfg.ip3_init) + '_' + str(cfg.gip3r)   # Set file output name
+# from neuron import h 
+# pc = h.ParallelContext()
+# pcid = pc.id()
+# cfg.filename = 'data/batchRxd/rxd_net_' + str(pcid) #+ str(cfg.ip3_init) + '_' + str(cfg.gip3r)   # Set file output name
 cfg.saveJson = True
-cfg.saveDataInclude = ['simData', 'simConfig', 'netParams', 'net']
+cfg.saveDataInclude = ['simData']
 
  # Network dimensions
 cfg.sizeX = 100
@@ -40,4 +40,7 @@ cfg.analysis['plotRaster'] = {'orderBy': 'y', 'orderInverse': True, 'saveFig': F
 cfg.analysis['plotLFP'] = {'includeAxon': False, 'figSize': (6,10), 'NFFT': 256, 'noverlap': 48, 'nperseg': 64, 'saveFig': False}
 # cfg.analysis['plotRxDConcentration'] = {'speciesLabel': 'ca', 'regionLabel': 'ecs', 'saveFig' : False}
 
-
+## Change ip3_init from 0 to 0.1 to observe multiscale effect:
+## high ip3 -> ER Ca released to Cyt -> kBK channels open -> less firing
+cfg.ip3_init = 0.0
+cfg.gip3r = 12040 * 100

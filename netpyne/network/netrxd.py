721ea1c529014fc12a1e94f1be28ea804fd03bb3
salvadord <salvadordura@gmail.com>
2021-06-14 16:27:12 -0400

    fixed rxd bug

--- netpyne/network/netrxd.py
+++ netpyne/network/netrxd.py
@@ -492,7 +492,7 @@ def _addRates(self, params):
 
     for label, param in params.items():
 
-        dynamicVars = {'sim': sim , 'rxdmath': rxdmath', 'rxd' : rxd}
+        dynamicVars = {'sim': sim, 'rxdmath': rxdmath, 'rxd' : rxd}
 
         # species
         if 'species' not in param:

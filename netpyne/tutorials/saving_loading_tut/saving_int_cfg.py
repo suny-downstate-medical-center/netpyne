eab56b056974a33d76626db9641e2e03d0554603
Joe Graham <joe.w.graham@gmail.com>
2021-06-23 10:09:08 -0400

    Added tutorial for saving (distibuted and interval) and loading

---
+++ netpyne/tutorials/saving_loading_tut/saving_int_cfg.py
@@ -0,0 +1,24 @@
+from netpyne import specs
+
+cfg = specs.SimConfig()
+
+# Saving parameters
+cfg.simLabel = 'saving_int'
+cfg.saveFolder = cfg.simLabel + '_data'
+cfg.savePickle = True
+cfg.saveDataInclude = ['simData', 'simConfig', 'netParams', 'net']
+
+# Simulation parameters
+cfg.duration = 10000
+cfg.dt = 0.025
+cfg.recordStep = 0.1
+
+# Analysis parameters
+cfg.recordTraces = {'V_soma': {'sec': 'soma','loc': 0.5,'var': 'v'}}
+cfg.analysis['plotRaster'] = {'saveFig': True} 
+cfg.analysis['plotTraces'] = {'include': [0], 'saveFig': True} 
+cfg.analysis['plotConn'] = {'saveFig': True}
+
+# Variable parameters (used in netParams)
+cfg.synMechTau2 = 5
+cfg.connWeight = 0.01

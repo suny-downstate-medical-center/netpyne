fcb0900bfc33e408d273ed4f5865817c69a7873b
Padraig Gleeson <p.gleeson@gmail.com>
2021-06-24 13:00:10 +0100

    Verbose example

--- examples/HHTut/HHTut_export.py
+++ examples/HHTut/HHTut_export.py
@@ -5,6 +5,7 @@ np = HHTut.netParams
 print("********************\n*\n*  Note: setting noise to 1, since noise can only be 0 or 1 in NeuroML export currently!\n*\n********************")
 np.stimSourceParams['bkg']['noise'] = 1
 
+HHTut.simConfig.verbose = True
 sim.createExportNeuroML2(netParams = np,
                        simConfig = HHTut.simConfig,
                        reference = 'HHTut')  # create and export network to NeuroML 2

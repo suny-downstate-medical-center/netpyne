e39aad756a39b3bbac8a27d0ac6f10aa99780468
salvadord <salvadordura@gmail.com>
2021-06-30 18:15:47 -0400

    comment out getCSD() call

--- netpyne/analysis/csd.py
+++ netpyne/analysis/csd.py
@@ -406,7 +406,7 @@ def plotCSD(CSD_data=None, LFP_input_data=None, overlay=None, timeRange=None, sa
     if CSD_data is None:
     
         from .. import sim
-        lfp_data, CSD_data, sampr, spacing_um, dt = getCSD(sampr=sampr, spacing_um=spacing_um, dt=dt, getAllData=True)
+        #lfp_data, CSD_data, sampr, spacing_um, dt = getCSD(sampr=sampr, spacing_um=spacing_um, dt=dt, getAllData=True)
 
         sim_data_categories = sim.allSimData.keys()
         

a5d824e0251f8633a66a70479cb3cd0f73096899
salvadord <salvadordura@gmail.com>
2021-12-04 13:05:15 -0500

    fixed distributed saving for batch

--- netpyne/sim/save.py
+++ netpyne/sim/save.py
@@ -603,8 +603,7 @@ def saveDataInNodes(filename=None, saveLFP=True, removeTraces=False, saveFolder=
         if getattr(sim.cfg, 'saveFolder', None) is None:
             saveFolder = sim.cfg.simLabel+'_node_data'
         else:
-            saveFolder = os.path.join(sim.cfg.saveFolder, 'node_data')
-            nodeDataDir = os.path.join(saveFolder, sim.cfg.simLabel+'_node_data')
+            saveFolder = os.path.join(saveFolder, sim.cfg.simLabel+'_node_data')
 
     if not os.path.exists(saveFolder):
         os.makedirs(saveFolder, exist_ok=True)

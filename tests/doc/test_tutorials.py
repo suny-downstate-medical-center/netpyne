b38794125f51e82bd125b471893e78aa1f7a8ad8
adam newton <adam.newton@yale.edu>
2022-01-04 10:41:56 -0500

    Added batch tests to CI. Other minor changes: examples/batchCell To get the mechanisms to compile; added ghk.inc from neurosim and moved misc.h into mod directory. Added erev as a GLOBAL to avoid an error.
    netpyne/batch/Batch.py -- allow a seed of 0

--- tests/doc/test_tutorials.py
+++ tests/doc/test_tutorials.py
@@ -1,29 +1,77 @@
 import sys
-if '-nogui' not in sys.argv:
-    sys.argv.append('-nogui')
-sys.path.append('doc/source/code/')
-sys.path.append('examples/HHTut/')
+import json
+import pytest
+
+if "-nogui" not in sys.argv:
+    sys.argv.append("-nogui")
+sys.path.append("doc/source/code/")
+sys.path.append("examples/HHTut/")
 import netpyne
+from multiprocessing import Process
 
 
 def test_tutorial_1():
     import tut1
 
+
 def test_tutorial_2():
     import tut2
 
+
 def test_tutorial_3():
     import tut3
+
 
 # TODO: Needs fixing
 # def test_tutorial_4():
 #     import tut4
 
+
 def test_tutorial_5():
     import tut5
+
 
 def test_tutorial_6():
     import tut6
 
+
 def test_tutorial_7():
     import tut7
+
+
+def batch_example():
+    """batch example from tutorial 8 with fewer parameters"""
+
+    from netpyne import specs
+    from netpyne.batch import Batch
+    import json
+
+    params = specs.ODict()
+    params["synMechTau2"] = [3.0]
+    params["connWeight"] = [0.005]
+    b = Batch(
+        params=params,
+        cfgFile="doc/source/code/tut8_cfg.py",
+        netParamsFile="doc/source/code/tut8_netParams.py",
+    )
+    b.batchLabel = "tauWeight"
+    b.saveFolder = "/tmp/tut8_data"
+    b.method = "grid"
+    b.runCfg = {
+        "type": "mpi_bulletin",
+        "script": "doc/source/code/tut8_init.py",
+        "skip": True,
+    }
+    # Run batch simulations
+    b.run()
+
+
+def test_tutorial_8():
+    """test tutorial 8 grid parameter search"""
+
+    p = Process(target=batch_example)
+    p.start()
+    p.join()
+    # check the output
+    test = json.load(open("/tmp/tut8_data/tauWeight_0_0_data.json", "r"))
+    assert test["simData"]["avgRate"] == 13.675

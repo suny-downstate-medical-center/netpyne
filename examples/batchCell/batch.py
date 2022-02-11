5c690bea78a13edf73c3be32ac9078348d9be8e1
salvadord <salvadordura@gmail.com>
2022-02-11 15:45:09 -0500

    fixed batchCell example

--- examples/batchCell/batch.py
+++ examples/batchCell/batch.py
@@ -43,9 +43,9 @@ def runBatchComet(b, label):
 
 
 def batchNa():
-	params = {'dendNa': [0.025],#, 0.03, 0.035, 0.4],
-			('IClamp1', 'amp'): [-2.0]} #list(np.arange(-2.0, 8.0, 0.5)/10.0)}
-	initCfg = {'duration': 1.1, 'tau1NMDA': 15}
+	params = {'dendNa': [0.025, 0.03, 0.035, 0.4],
+			('IClamp1', 'amp'): list(np.arange(-2.0, 8.0, 0.5)/10.0)}
+	initCfg = {'duration': 30*1e3, 'tau1NMDA': 15}
 	b = Batch(params=params, initCfg=initCfg)
 	runBatch(b, 'batchNa', setup='mpi_bulletin')
 
--- examples/batchCell/init.py
+++ examples/batchCell/init.py
@@ -23,10 +23,11 @@ sim.net.connectCells()            			# create connections between cells based on
 sim.net.addStims() 							# add network stimulation
 sim.setupRecording()              			# setup variables to record for each cell (spikes, V traces, etc)
 sim.runSim()                      			# run parallel Neuron simulation  
-#sim.gatherData()                  			# gather spiking data and cell info from each node
+sim.gatherData()                  			# gather spiking data and cell info from each node
 
 # distributed saving (to avoid errors with large output data)
-sim.saveDataInNodes()
-sim.gatherDataFromFiles(saveMerged=True)
+#sim.saveDataInNodes()
+#sim.gatherDataFromFiles(saveMerged=True)
 
+sim.saveData()                  			# gather spiking data and cell info from each node
 sim.analysis.plotData()        

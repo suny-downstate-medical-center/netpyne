e3addebffa952adb9f5e880e364da7fababf8b40
James <jchen.6727@gmail.com>
2023-03-07 22:19:54 -0500

    doc for stim

--- netpyne/network/stim.py
+++ netpyne/network/stim.py
@@ -25,6 +25,17 @@ except NameError:
 def addStims(self):
     """
     Function for/to <short description of `netpyne.network.stim.addStims`>
+    Usage:
+        Creates and attaches stims to targets via CompartCell.addstim() based on entries in the specs.NetParams()
+        dictionaries (see below)
+        NetParams.stimSourceParams entries contain key-value pairs to describe NEURON point processes specified by the
+        'type' entry (i.e. 'IClamp', 'VClamp', 'SEClamp', 'AlphaSynapse')
+        NetParams.stimTargetParams entries contain key-value pairs to describe the post-synaptic connections for a
+        stimSourceParam entry specified by the 'source' entry, including a 'sec' and 'loc' entry (describing section
+        and location) for where the post-synaptic connection will exist and a 'conds' entry with a dictionary
+        specifying the cell criteria for the post-synaptic connections: (i.e. 'x', 'y', 'z' or 'xnorm', 'ynorm', 'znorm'
+        specifying cell criteria by location, 'cellList' specifying cell criteria by specific gid, or arbitrary
+        'key': 'value' tags.
 
     Parameters
     ----------

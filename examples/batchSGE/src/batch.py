340f42290644420c88149225bed4cdf6a53966df
jchen6727 <jchen.6727@gmail.com>
2023-06-06 12:55:14 -0400

    Example of running batch jobs on SGE (#756)
    * tutorial for running tutorial 8 on SGE, see README.md

---
+++ examples/batchSGE/src/batch.py
@@ -0,0 +1,30 @@
+from netpyne import specs
+from netpyne.batch import Batch
+
+def batchTauWeight():
+        # Create variable of type ordered dictionary (NetPyNE's customized version)
+        params = specs.ODict()
+
+        # fill in with parameters to explore and range of values (key has to coincide with a variable in simConfig)
+        params['synMechTau2'] = [3.0, 5.0, 7.0]
+        params['connWeight'] = [0.005, 0.01, 0.15]
+
+        # create Batch object with parameters to modify, and specifying files to use
+        b = Batch(params=params, cfgFile='cfg.py', netParamsFile='params.py',)
+
+        # Set output folder, grid method (all param combinations), and run configuration
+        b.batchLabel = 'tauWeight'
+        b.saveFolder = 'tut8_data'
+        b.method = 'grid'
+        b.runCfg = {'type': 'hpc_sge',
+                    'jobName': 'my_batch',
+                    'cores': 4,
+                    'script': 'init.py',
+                    'skip': True}
+
+        # Run batch simulations
+        b.run()
+
+# Main code
+if __name__ == '__main__':
+        batchTauWeight()

e1691256131dfd8b2380c49f5b21427d51f1ebbc
jchen_lethe <jchen.6727@gmail.com>
2025-05-06 17:41:34 -0400

    modified to init.py to clarify sim.send with new data parsing

--- netpyne/batchtools/examples/CA3/init.py
+++ netpyne/batchtools/examples/CA3/init.py
@@ -24,21 +24,4 @@ if sim.rank == 0:
 
 sim.send(data)
 
-"""
-if sim.rank == 0:
-    netParams.save("{}/{}_params.json".format(cfg.saveFolder, cfg.simLabel))
-    print('transmitting data...')
-    inputs = cfg.get_mappings()
-    #print(json.dumps({**inputs}))
-    results = sim.analysis.popAvgRates(show=False)
-
-    results['PYR_loss'] = (results['PYR'] - 3.33875)**2
-    results['BC_loss']  = (results['BC']  - 19.725 )**2
-    results['OLM_loss'] = (results['OLM'] - 3.470  )**2
-    results['loss'] = (results['PYR_loss'] + results['BC_loss'] + results['OLM_loss']) / 3
-    out_json = json.dumps({**inputs, **results})
-
-    print(out_json)
-    sim.send(out_json)
-"""
 

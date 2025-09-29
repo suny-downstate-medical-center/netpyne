2a089a2421a84c85f5bc78b75819bd2ecb0c179c
James Chen <jchen.6727@gmail.com>
2025-09-29 15:32:30 -0500

    new example to development (with new batchtk development)

---
+++ netpyne/batchtools/examples/optuna_search/single_opt.py
@@ -0,0 +1,15 @@
+from netpyne import sim, specs
+
+A = 1
+cfg = specs.SimConfig()
+
+cfg.x0 = 0.0
+cfg.x1 = 0.0
+
+cfg.update()
+
+def rosenbrock(x0, x1):
+    return 100 * (x1 - x0**2)**2 + (A - x0)**2
+
+result = rosenbrock(cfg.x0, cfg.x1)
+sim.send({'fx': result})

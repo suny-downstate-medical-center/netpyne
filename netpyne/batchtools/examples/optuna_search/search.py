72647a5884518ef6660f519198519ee655ea7705
James Chen <jchen.6727@gmail.com>
2025-10-01 13:26:04 -0500

    search with slurm args

--- netpyne/batchtools/examples/optuna_search/search.py
+++ netpyne/batchtools/examples/optuna_search/search.py
@@ -3,7 +3,22 @@ from batchtk.utils import expand_path
 
 from netpyne.batchtools.search import generate_constructors
 
+#option for local run
+# dispatcher, submit = generate_constructors('sh', 'sfs')
+
+#option for slurm run
 dispatcher, submit = generate_constructors('sh', 'sfs')
+slurm_args = {
+    'allocation': 'csd403',
+    'realtime': '00:30:00',
+    'nodes': '1',
+    'coresPerNode': '1',
+    'mem': '4G',
+    'partition': 'shared',
+    'email': 'jchen.6727@gmail.com',
+    'custom': '',
+    'command': 'python single_opt.py',
+}
 
 results = optuna_search(
     study_label='rosenbrock',
@@ -12,7 +27,8 @@ results = optuna_search(
     num_trials=12, num_workers=3,
     dispatcher_constructor=dispatcher,
     submit_constructor=submit,
-    submit_kwargs={'command': 'python single_opt.py'},
+    #submit_kwargs={'command': 'python single_opt.py'}, # normal run
+    submit_kwargs=slurm_args,
     interval=10,
     project_path='.',
     output_path=expand_path('./optimization', create_dirs=True),

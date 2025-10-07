d2f273d5ba529ddc7d8df2428034f3e39dda709a
James Chen <jchen.6727@gmail.com>
2025-10-07 17:31:20 -0500

    update to runner and single_opt
    validate batch saving w/o fileclobber

--- netpyne/batchtools/examples/optuna_search/single_opt.py
+++ netpyne/batchtools/examples/optuna_search/single_opt.py
@@ -6,10 +6,18 @@ cfg = specs.SimConfig()
 cfg.x0 = 0.0
 cfg.x1 = 0.0
 
+cfg.saveDataInclude = ['simConfig']
+cfg.saveJson = True
 cfg.update()
 
+print(cfg.simLabel)
+print(cfg.saveFolder)
 def rosenbrock(x0, x1):
     return 100 * (x1 - x0**2)**2 + (A - x0)**2
 
 result = rosenbrock(cfg.x0, cfg.x1)
+sim.initialize(netParams={}, simConfig=cfg)
+sim.saveData()
+
 sim.send({'fx': result})
+
